a6970ffa560ee774a4b50dc45d7d7fe890802227
Evgeny Semenov <00semen00@gmail.com>
2025-01-11 11:29:24 +0300

    Solved problem 83 from leetcode "Remove Duplicates from Sorted List"
    https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/

--- src/leetcode21/Leetcode83.java
+++ src/leetcode21/Leetcode83.java
@@ -2,84 +2,49 @@ package leetcode21;
 
 public class Leetcode83 {
 
+/*    Input: head = [1,1,2]
+    Output: [1,2]
+
+    Input: head = [1,1,2,3,3]
+    Output: [1,2,3]*/
+
     public static void main(String[] args) {
         Leetcode83 leetcode = new Leetcode83();
-        ListNode head = new ListNode(2);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(4);
-        ListNode head2 = new ListNode(1);
-        head2.next = new ListNode(3);
-        head2.next.next = new ListNode(4);
-        System.out.println(leetcode.mergeTwoLists(head, head2));
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
 
-        head = new ListNode(2);
-        head2 = new ListNode(1);
-        System.out.println(leetcode.mergeTwoLists(head, head2));
+        ListNode head2 = new ListNode(1);
+        head2.next = new ListNode(1);
+        head2.next.next = new ListNode(2);
+        head2.next.next.next = new ListNode(3);
+        head2.next.next.next.next = new ListNode(3);
+
+        ListNode head3 = null;
+
+        System.out.println(leetcode.deleteDuplicates(head));
+        System.out.println(leetcode.deleteDuplicates(head2));
+        System.out.println(leetcode.deleteDuplicates(head3));
     }
 
     public ListNode deleteDuplicates(ListNode head) {
-
-    }
-
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null) {
-            return list2;
+        if (head == null || head.next == null) {
+            return head;
         }
-        if (list2 == null) {
-            return list1;
-        }
-
-        ListNode result = new ListNode(0);
+        ListNode result = new ListNode(-1000);
         ListNode last = result;
-        ListNode head = list1;
-        ListNode head2 = list2;
-        boolean first = true;
-        while (head != null && head2 != null) {
-            if (head.val == head2.val) {
-                if (first) {
+        int lastVal = -1000;
+        while (head != null) {
+            if (last.val != head.val && head.val != lastVal) {
+                if (last.val == -1000) {
                     last.val = head.val;
                 } else {
                     last.next = new ListNode(head.val);
                     last = last.next;
                 }
-                last.next = new ListNode(head2.val);
-                head = head.next;
-                head2 = head2.next;
-                last = last.next;
-            } else if (head.val > head2.val) {
-                if (first) {
-                    last.val = head2.val;
-                } else {
-                    last.next = new ListNode(head2.val);
-                    last = last.next;
-                }
-                head2 = head2.next;
-            } else {
-                if (first) {
-                    last.val = head.val;
-                } else {
-                    last.next = new ListNode(head.val);
-                    last = last.next;
-                }
-                head = head.next;
             }
-            if (first) {
-                first = false;
-            }
-        }
-        if (head != null) {
-            while (head != null) {
-                last.next = new ListNode(head.val);
-                last = last.next;
-                head = head.next;
-            }
-        }
-        if (head2 != null) {
-            while (head2 != null) {
-                last.next = new ListNode(head2.val);
-                last = last.next;
-                head2 = head2.next;
-            }
+            lastVal = head.val;
+            head = head.next;
         }
         return result;
     }
