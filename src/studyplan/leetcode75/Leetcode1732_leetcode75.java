a123214906855e196c3ec42e333876f2b94d5a21
Evgeny Semenov <00semen00@gmail.com>
2025-01-20 20:50:00 +0300

    Solved problem 1732 from leetcode "Find the Highest Altitude"
    https://leetcode.com/problems/find-the-highest-altitude/description/?envType=study-plan-v2&envId=leetcode-75

--- src/studyplan/leetcode75/Leetcode1732_leetcode75.java
+++ src/studyplan/leetcode75/Leetcode1732_leetcode75.java
@@ -15,8 +15,8 @@ Example 1:
 Input: gain = [-5,1,5,0,-7]
 Output: 1
 Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
-Example 2:
 
+Example 2:
 Input: gain = [-4,-3,-2,-1,4,3,2]
 Output: 0
 Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
@@ -32,95 +32,52 @@ n == gain.length
     public static void main(String[] args) {
         Leetcode1732_leetcode75 solution = new Leetcode1732_leetcode75();
         long startDate = new Date().getTime();
-        int[] nums = new int[]{1, 1, 0, 1};
-        System.out.println(solution.longestSubarray(nums) + " milliseconds:" + (new Date().getTime() - startDate));
+        int[] nums = new int[]{-5, 1, 5, 0, -7};
+        solution.printNums(nums);
+        System.out.println(solution.largestAltitude(nums) + " milliseconds:" + (new Date().getTime() - startDate));
         System.out.println("-----");
-        //Output: 3
-
+        //Output: 1
         startDate = new Date().getTime();
-        int[] nums2 = new int[]{0, 1, 1, 1, 0, 1, 1, 0, 1};
-        System.out.println(solution.longestSubarray(nums2) + " milliseconds:" + (new Date().getTime() - startDate));
-        System.out.println("-----");
-        //Output: 5
-
-        startDate = new Date().getTime();
-        int[] nums3 = new int[]{1, 1, 1};
-        System.out.println(solution.longestSubarray(nums3) + " milliseconds:" + (new Date().getTime() - startDate));
-        System.out.println("-----");
-        //Output: 2
-
-        startDate = new Date().getTime();
-        int[] nums4 = new int[]{1, 0, 1};
-        System.out.println(solution.longestSubarray(nums4) + " milliseconds:" + (new Date().getTime() - startDate));
-        System.out.println("-----");
-        //Output: 2
-
-        startDate = new Date().getTime();
-        int[] nums5 = new int[]{1};
-        System.out.println(solution.longestSubarray(nums5) + " milliseconds:" + (new Date().getTime() - startDate));
+        int[] nums2 = new int[]{-4,-3,-2,-1,4,3,2};
+        solution.printNums(nums2);
+        System.out.println(solution.largestAltitude(nums2) + " milliseconds:" + (new Date().getTime() - startDate));
         System.out.println("-----");
         //Output: 0
-
         startDate = new Date().getTime();
-        int[] nums6 = new int[100000];
-        for (int i = 0; i < 100000; i++) {
-            nums6[i] = i % 2;
+        int[] nums3 = new int[100];
+        Arrays.fill(nums3, 100);
+        solution.printNums(nums3);
+        System.out.println(solution.largestAltitude(nums3) + " milliseconds:" + (new Date().getTime() - startDate));
+        System.out.println("-----");
+        //Output: 10000
+        int[] nums4 = new int[100];
+        Arrays.fill(nums4, -100);
+        solution.printNums(nums4);
+        System.out.println(solution.largestAltitude(nums4) + " milliseconds:" + (new Date().getTime() - startDate));
+        System.out.println("-----");
+        //Output: 0
+        int[] nums5 = new int[100];
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 == 0) {
+                nums5[i] = 100;
+            } else {
+                nums5[i] = -100;
+            }
         }
-        solution.printNums(nums6);
-        System.out.println(solution.longestSubarray(nums6) + " milliseconds:" + (new Date().getTime() - startDate));
+        solution.printNums(nums5);
+        System.out.println(solution.largestAltitude(nums5) + " milliseconds:" + (new Date().getTime() - startDate));
         System.out.println("-----");
-        //Output: 3
-
-        startDate = new Date().getTime();
-        int[] nums7 = new int[100000];
-        Arrays.fill(nums7, 1);
-        solution.printNums(nums7);
-        System.out.println(solution.longestSubarray(nums7) + " milliseconds:" + (new Date().getTime() - startDate));
-        System.out.println("-----");
-        //Output: 99999
-
-        startDate = new Date().getTime();
-        int[] nums8 = new int[100000];
-        Arrays.fill(nums8, 1);
-        nums8[50000] = 0;
-        solution.printNums(nums8);
-        System.out.println(solution.longestSubarray(nums8) + " milliseconds:" + (new Date().getTime() - startDate));
-        System.out.println("-----");
-        //Output: 99999
+        //Output: 100
     }
 
     public int largestAltitude(int[] gain) {
         int result = 0;
-        return result;
-    }
-
-    public int longestSubarray(int[] nums) {
-        int result = -1;
-        int count;
-        int countToFlip = 1;
-        for (int i = 0; i < nums.length; i++) {
-            if (result >= nums.length - i) {
-                break;
+        int altitude = 0;
+        for (int j : gain) {
+            altitude += j;
+            if (altitude > result) {
+                result = altitude;
             }
-            count = 0;
-            for (int j = i; j < nums.length; j++) {
-                if (nums[j] == 1) {
-                    count++;
-                } else {
-                    if (countToFlip == 0) {
-                        break;
-                    } else {
-                        countToFlip--;
-                    }
-                }
-            }
-            countToFlip = 1;
-            if (count > result) {
-                result = count;
-            }
-        }
-        if (result == nums.length) {
-            result--;
         }
         return result;
     }
