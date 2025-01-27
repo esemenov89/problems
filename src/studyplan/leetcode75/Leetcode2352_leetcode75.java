092f1e8b212bef380866411e03f76f9cc6a236ea
Evgeny Semenov <00semen00@gmail.com>
2025-01-27 21:29:48 +0300

    Solved problem 2352 from leetcode "Equal Row and Column Pairs"
    https://leetcode.com/problems/equal-row-and-column-pairs/description/?envType=study-plan-v2&envId=leetcode-75

--- src/studyplan/leetcode75/Leetcode2352_leetcode75.java
+++ src/studyplan/leetcode75/Leetcode2352_leetcode75.java
@@ -1,5 +1,6 @@
 package studyplan.leetcode75;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -41,18 +42,77 @@ n == grid.length == grid[i].length
         long startDate = new Date().getTime();
         int[][] grid = new int[][]{{3,2,1},{1,7,6},{2,7,7}};
         System.out.println("grid");
+        solution.printGrid(grid);
         System.out.println(solution.equalPairs(grid) + " milliseconds:" + (new Date().getTime() - startDate));
         System.out.println("-----");
-        //Output: true
+        //Output: 1
+        startDate = new Date().getTime();
+        int[][] grid2 = new int[][]{{3,1,2,2},{1,4,4,5},{2,4,2,2},{2,4,2,2}};
+        System.out.println("grid2");
+        solution.printGrid(grid2);
+        System.out.println(solution.equalPairs(grid2) + " milliseconds:" + (new Date().getTime() - startDate));
+        System.out.println("-----");
+        //Output: 3
+        startDate = new Date().getTime();
+        int[][] grid3 = new int[200][200];
+        System.out.println("grid3");
+        for (int[] ints : grid3) {
+            Arrays.fill(ints, 100000);
+        }
+        solution.printGrid(grid3);
+        System.out.println(solution.equalPairs(grid3) + " milliseconds:" + (new Date().getTime() - startDate));
+        System.out.println("-----");
+        //Output: 40000
+        startDate = new Date().getTime();
+        int[][] grid4 = new int[][]{{1}};
+        System.out.println("grid4");
+        solution.printGrid(grid4);
+        System.out.println(solution.equalPairs(grid4) + " milliseconds:" + (new Date().getTime() - startDate));
+        System.out.println("-----");
+        //Output: 1
+        startDate = new Date().getTime();
+        int[][] grid5 = new int[][]{{11,1}, {1,11}};
+        System.out.println("grid5");
+        solution.printGrid(grid5);
+        System.out.println(solution.equalPairs(grid5) + " milliseconds:" + (new Date().getTime() - startDate));
+        System.out.println("-----");
+        //Output: 1
     }
 
     public int equalPairs(int[][] grid) {
         int result = 0;
+        Map<Integer, String> rowsMap = new HashMap<>();
+        Map<Integer, String> columnsMap = new HashMap<>();
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                System.out.println(grid[i][j]);
+                if (j == 0) {
+                    rowsMap.put(i, String.valueOf(grid[i][j]));
+                } else {
+                    rowsMap.put(i, rowsMap.get(i) + "-" + grid[i][j]);
+                }
+                if (i == 0) {
+                    columnsMap.put(j, String.valueOf(grid[i][j]));
+                } else {
+                    columnsMap.put(j, columnsMap.get(j) + "-" + grid[i][j]);
+                }
+            }
+        }
+        for (int i = 0; i < rowsMap.size(); i++) {
+            for (int j = 0; j < columnsMap.size(); j++) {
+                if (rowsMap.get(i).equals(columnsMap.get(j))) {
+                    result++;
+                }
             }
         }
         return result;
+    }
+
+    private void printGrid(int[][] grid) {
+        for (int[] ints : grid) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
+            }
+            System.out.println();
+        }
     }
 }
