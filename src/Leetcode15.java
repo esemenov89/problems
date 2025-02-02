1d3c70582ef2822bb0a8016e9effe5b1fcc856f8
Evgeny Semenov <00semen00@gmail.com>
2025-02-02 22:29:50 +0300

    Continue problem 15 from leetcode "3Sum"
    https://leetcode.com/problems/3sum/description/?envType=problem-list-v2&envId=array

--- src/Leetcode15.java
+++ src/Leetcode15.java
@@ -55,8 +55,8 @@ Constraints:
         startDate = new Date().getTime();
         int[] nums3 = new int[]{0, 0, 0};
         System.out.println("nums3: " + Arrays.toString(nums3));
-        List<List<Integer>> resul3 = solution.threeSum(nums3);
-        System.out.println(resul3 + " milliseconds:" + (new Date().getTime() - startDate));
+/*        List<List<Integer>> resul3 = solution.threeSum(nums3);
+        System.out.println(resul3 + " milliseconds:" + (new Date().getTime() - startDate));*/
         // [[0, 0, 0]]
         startDate = new Date().getTime();
         int[] nums4 = new int[3000];
@@ -73,31 +73,54 @@ Constraints:
         List<List<Integer>> resul4 = solution.threeSum(nums4);
         System.out.println(resul4 + " milliseconds:" + (new Date().getTime() - startDate));
         // [[-1, 1, 0]]
+        startDate = new Date().getTime();
+        int[] nums5 = new int[]{-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0};
+        System.out.println("nums1: " + Arrays.toString(nums5));
+/*        List<List<Integer>> result = solution.threeSum(nums5);
+        System.out.println(result + " milliseconds:" + (new Date().getTime() - startDate));*/
+        // [[-5,1,4],[-4,0,4],[-4,1,3],[-2,-2,4],[-2,1,1],[0,0,0]]
     }
 
+    // TODO: Не прошел по скорости 308 кейс, подумать как ускорить
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         Set<String> set = new HashSet<>();
-        System.out.println("before triple");
         for (int i = 0; i < nums.length - 2; i++) {
-            System.out.println(i);
             for (int j = i + 1; j < nums.length - 1; j++) {
+                if (set.contains(nums[i] + "=" + nums[j]) || set.contains(nums[j] + "=" + nums[i])) {
+                    continue;
+                }
                 for (int k = j + 1; k < nums.length; k++) {
-                    if (nums[i] + nums[j] + nums[k] == 0 && !set.contains(nums[i] + "=" + nums[j] + "=" + nums[k])) {
+/*                    if (set.contains(nums[i] + "=" + nums[j]) || set.contains(nums[j] + "=" + nums[i]) ||
+                            set.contains(nums[i] + "=" + nums[k]) || set.contains(nums[k] + "=" + nums[i]) ||
+                            set.contains(nums[j] + "=" + nums[k]) || set.contains(nums[k] + "=" + nums[j])) {
+                        continue;
+                    }*/
+                    if (nums[i] + nums[j] + nums[k] == 0) {
                         result.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                        set.add(nums[i] + "=" + nums[j] + "=" + nums[k]);
+                        set.add(nums[i] + "=" + nums[j]);
+                        set.add(nums[i] + "=" + nums[k]);
+                        set.add(nums[j] + "=" + nums[k]);
                     }
                 }
             }
         }
-        System.out.println("before remove");
         for (int i = 0; i < result.size(); i++) {
             for (int j = i + 1; j < result.size(); j++) {
-                if (result.get(i).containsAll(result.get(j))) {
+                if (result.get(i).contains(result.get(j).get(0)) && result.get(i).contains(result.get(j).get(1))
+                        && result.get(i).contains(result.get(j).get(2)) && result.get(j).contains(result.get(i).get(0))
+                        && result.get(j).contains(result.get(i).get(1)) && result.get(j).contains(result.get(i).get(2))) {
                     result.remove(j);
+                    if (i > 0) {
+                        i--;
+                    }
+                    if (j > 0) {
+                        j--;
+                    }
                 }
             }
         }
         return result;
     }
+
 }
