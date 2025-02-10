c21d21a5aa7dc5539d8c1e12400a3140a51da5c2
Evgeny Semenov <00semen00@gmail.com>
2025-02-10 18:52:57 +0300

    Started problem 1961 from leetcode "Check If String Is a Prefix of Array"
    https://leetcode.com/problems/check-if-string-is-a-prefix-of-array/description/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode1961/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode1961;
+
+public class Solution {
+
+    public boolean isPrefixString(String s, String[] words) {
+        boolean result = true;
+        return result;
+    }
+}
