e3d4e23e31edc477d6b7c802cdd459cefe8d98c5
Evgeny Semenov <00semen00@gmail.com>
2025-02-15 16:29:39 +0300

    Started problem 328 from leetcode "Odd Even Linked List"
    https://leetcode.com/problems/odd-even-linked-list/description/?envType=study-plan-v2&envId=leetcode-75

---
+++ src/main/java/org/just/leetcode/study/plan/leetcode75/leetcode328/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.study.plan.leetcode75.leetcode328;
+
+public class Solution {
+
+    public ListNode oddEvenList(ListNode head) {
+        ListNode result = head;
+        return result;
+    }
+}
