1fc26468f4a43fa25241cd9aa4c6529d4d3b9313
Evgeny Semenov <00semen00@gmail.com>
2025-02-16 11:34:05 +0300

    Solved problem 104 from leetcode "Maximum Depth of Binary Tree"
    https://leetcode.com/problems/maximum-depth-of-binary-tree/description/?envType=study-plan-v2&envId=leetcode-75

--- src/main/java/org/just/leetcode/study/plan/leetcode75/leetcode104/Solution.java
+++ src/main/java/org/just/leetcode/study/plan/leetcode75/leetcode104/Solution.java
@@ -1,33 +1,22 @@
 package org.just.leetcode.study.plan.leetcode75.leetcode104;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class Solution {
 
+    int maxDepth = 0;
+
     public int maxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
+        traversePreOrder(root, 0);
+        return maxDepth;
+    }
+
+    private void traversePreOrder(TreeNode node, int sum) {
+        if (node != null) {
+            sum++;
+            if (sum > maxDepth) {
+                maxDepth = sum;
+            }
+            traversePreOrder(node.left, sum);
+            traversePreOrder(node.right, sum);
         }
-        int result = 1;
-        TreeNode temp;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            temp = queue.poll();
-            if (temp.left != null || temp.right != null) {
-                result++;
-            }
-            if (temp.left != null && temp.right != null) {
-                result--;
-            }
-            if (temp.left != null) {
-                queue.add(temp.left);
-            }
-            if (temp.right != null) {
-                queue.add(temp.right);
-            }
-        }
-        return result;
     }
 }
