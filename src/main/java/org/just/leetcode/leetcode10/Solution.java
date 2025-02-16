5b47bec4e289358c889741b014bc691bc76b30cb
Evgeny Semenov <00semen00@gmail.com>
2025-02-16 16:25:23 +0300

    Continue problem 10 from leetcode "Regular Expression Matching"
    https://leetcode.com/problems/regular-expression-matching/description/?envType=problem-list-v2&envId=string

--- src/main/java/org/just/leetcode/leetcode10/Solution.java
+++ src/main/java/org/just/leetcode/leetcode10/Solution.java
@@ -15,15 +15,33 @@ public class Solution {
         for (int i = 0; i < charsS.length; i++) {
             if (i > 0 && charsS[i] != charsS[i - 1]) {
                 endInterval = i;
-                if (!validInterval(s.substring(startInterval, endInterval), j)) {
+                if (!validInterval(s.substring(startInterval, endInterval), j,charsP)) {
                     return false;
                 }
             }
         }
         return result;
     }
-    private boolean validInterval(String s, int j) {
+    private boolean validInterval(String s, int j, char[] p) {
         boolean result = true;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == p[j] || p[j] == '.') {
+                j++;
+            } else {
+                if (p[j] == '*') {
+                    if (p[j - 1] == s.charAt(i) || p[j - 1] == '.') {
+                        if (j < p.length - 1) {
+                            if (p[j + 1] == s.charAt(i)) {
+                                while (p[j] == s.charAt(i) && j < p.length - 1) {
+                                    j++;
+                                }
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
         return result;
     }
 }
