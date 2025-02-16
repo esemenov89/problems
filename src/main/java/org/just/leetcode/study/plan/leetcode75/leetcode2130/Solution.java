4e4e9adc52b44dffbe43f3cfc718ae4957bdf393
Evgeny Semenov <00semen00@gmail.com>
2025-02-16 09:45:59 +0300

    Started problem 2130 from leetcode "Maximum Twin Sum of a Linked List"
    https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/?envType=study-plan-v2&envId=leetcode-75

---
+++ src/main/java/org/just/leetcode/study/plan/leetcode75/leetcode2130/Solution.java
@@ -0,0 +1,49 @@
+package org.just.leetcode.study.plan.leetcode75.leetcode2130;
+
+public class Solution {
+
+    public int pairSum(ListNode head) {
+        int result = 0;
+        return result;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode copyOfHead = head;
+        ListNode result = new ListNode(head.val);
+        ListNode last = result;
+        int sizeOfListNode = 1;
+        int iterations = -1;
+        int innerIterations = -1;
+        while (iterations != 0) {
+            while (innerIterations != 0) {
+                head = head.next;
+                sizeOfListNode++;
+                innerIterations--;
+                if (head.next == null || innerIterations == 1) {
+                    if (iterations == -1) {
+                        last.val = head.val;
+                    } else {
+                        last.next = new ListNode(head.val);
+                        last = last.next;
+                    }
+                    break;
+                }
+            }
+            if (iterations == -1) {
+                iterations = sizeOfListNode - 1;
+            } else {
+                iterations--;
+            }
+            innerIterations = iterations;
+            head = copyOfHead;
+            if (innerIterations == 1) {
+                last.next = new ListNode(head.val);
+                break;
+            }
+        }
+        return result;
+    }
+}
