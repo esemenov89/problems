92580083b386235aea4f65e2600837c13074db69
Evgeny Semenov <00semen00@gmail.com>
2025-02-16 10:19:35 +0300

    Started problem 104 from leetcode "Maximum Depth of Binary Tree"
    https://leetcode.com/problems/maximum-depth-of-binary-tree/description/?envType=study-plan-v2&envId=leetcode-75

---
+++ src/main/java/org/just/leetcode/study/plan/leetcode75/leetcode104/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.study.plan.leetcode75.leetcode104;
+
+public class Solution {
+
+    public int maxDepth(TreeNode root) {
+        int result = 0;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/study/plan/leetcode75/leetcode104/SolutionTest.java
@@ -0,0 +1,89 @@
+package org.just.leetcode.study.plan.leetcode75.leetcode104;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideStrings")
+    void oddEvenList(TreeNode root, int expected) {
+        int actual = solution.maxDepth(root);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideStrings() {
+        return Stream.of(
+                Arguments.of(generateListNode1(), generateListNode1Result()),
+                Arguments.of(generateListNode2(), generateListNode2Result()),
+                Arguments.of(generateListNode3(), generateListNode3Result()),
+                Arguments.of(new ListNode(1), new ListNode(1)),
+                Arguments.of(null, null),
+                Arguments.of(generateListNode(), new ListNode(1))
+        );
+    }
+
+    private static TreeNode generateTreeNode1() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        return head;
+    }
+
+    private static ListNode generateListNode1Result() {
+        ListNode head = new ListNode(5);
+        head.next = new ListNode(4);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(2);
+        head.next.next.next.next = new ListNode(1);
+        return head;
+    }
+
+    private static ListNode generateListNode2() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        return head;
+    }
+
+    private static ListNode generateListNode2Result() {
+        ListNode head = new ListNode(2);
+        head.next = new ListNode(1);
+        return head;
+    }
+
+    private static ListNode generateListNode3() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        return head;
+    }
+
+    private static ListNode generateListNode3Result() {
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(1);
+        return head;
+    }
+
+    private static ListNode generateListNode() {
+        ListNode head = new ListNode(1);
+        ListNode last = head;
+        for (int i = 0; i < 5000; i++) {
+            last.next = new ListNode(i + 1);
+            last = last.next;
+        }
+        return head;
+    }
+}
