f53f5d5319063c358067bd8c21111fecdba97120
Evgeny Semenov <00semen00@gmail.com>
2025-02-24 16:29:37 +0300

    Solved problem 1769 from leetcode "Minimum Number of Operations to Move All Balls to Each Box"
    https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/?envType=problem-list-v2&envId=array

--- src/main/java/org/just/leetcode/leetcode1769/Solution.java
+++ src/main/java/org/just/leetcode/leetcode1769/Solution.java
@@ -4,8 +4,16 @@ public class Solution {
 
     public int[] minOperations(String boxes) {
         int[] result = new int[boxes.length()];
-        for (int i = 0; i < boxes.length(); i++) {
-
+        char[] boxesChars = boxes.toCharArray();
+        for (int i = 0; i < boxesChars.length; i++) {
+            for (int j = 0; j < boxesChars.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (boxesChars[j] == '1') {
+                    result[i] += Math.abs(j - i);
+                }
+            }
         }
         return result;
     }
--- src/test/java/org/just/leetcode/leetcode1769/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode1769/SolutionTest.java
@@ -6,8 +6,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.just.leetcode.leetcode1929.Solution;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class SolutionTest {
@@ -17,35 +17,23 @@ public class SolutionTest {
     @ParameterizedTest
     @MethodSource("provideArguments")
     void testSolution(String arg1, int[] expected) {
-        int[] actual = solution.getConcatenation(arg1);
+        int[] actual = solution.minOperations(arg1);
 
         assertThat(actual, is(expected));
     }
 
     private static Stream<Arguments> provideArguments() {
         return Stream.of(
-                Arguments.of(new int[]{1, 2, 1}, new int[]{1, 2, 1, 1, 2, 1}),
-                Arguments.of(new int[]{1, 3, 2, 1}, new int[]{1, 3, 2, 1, 1, 3, 2, 1}),
-                Arguments.of(generateArray(), generateResultArray())
+                Arguments.of("110", new int[]{1, 1, 3}),
+                Arguments.of("001011", new int[]{11, 8, 5, 4, 3, 4}),
+                Arguments.of("111", new int[]{3, 2, 3}),
+                Arguments.of("1".repeat(2000), generateResultArray())
         );
-    }
-
-    private static int[] generateArray() {
-        int[] array = new int[1000];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = i;
-        }
-        return array;
     }
 
     private static int[] generateResultArray() {
         int[] array = new int[2000];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = i;
-            if (i >= 1000) {
-                array[i] = i - 1000;
-            }
-        }
+        Arrays.fill(array, 1999);
         return array;
     }
 }
