7fca10bcdb7084ff3442c26eaebf668d70b7fd40
Evgeny Semenov <00semen00@gmail.com>
2025-03-07 09:21:20 +0300

    Started problem 535 from leetcode "Encode and Decode TinyURL"
    https://leetcode.com/problems/encode-and-decode-tinyurl/description/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode535/Solution.java
@@ -0,0 +1,24 @@
+package org.just.leetcode.leetcode535;
+
+public class Solution {
+
+    public int countConsistentStrings(String allowed, String[] words) {
+        int result = 0;
+        char[] chars;
+        boolean containAll;
+        for (String word : words) {
+            chars = word.toCharArray();
+            containAll = true;
+            for (char c : chars) {
+                if (allowed.indexOf(c) == -1) {
+                    containAll = false;
+                    break;
+                }
+            }
+            if (containAll) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
