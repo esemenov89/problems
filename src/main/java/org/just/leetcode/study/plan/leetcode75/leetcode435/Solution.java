d6ffc6edf2db94e1a2689d165125136995bfeb9e
Evgeny Semenov <00semen00@gmail.com>
2025-03-16 11:46:24 +0300

    Continue problem 437 from leetcode "Path Sum III"
    https://leetcode.com/problems/path-sum-iii/description/?envType=study-plan-v2&envId=leetcode-75

--- src/main/java/org/just/leetcode/study/plan/leetcode75/leetcode435/Solution.java
+++ src/main/java/org/just/leetcode/study/plan/leetcode75/leetcode435/Solution.java
@@ -5,18 +5,41 @@ import java.util.List;
 
 public class Solution {
 
+    private List<List<Integer>> lists = new ArrayList<>();
+
     public int pathSum(TreeNode root, int targetSum) {
         int result = 0;
         List<Integer> nodesValue = new ArrayList<>();
+        lists.add(nodesValue);
         iterate(root,nodesValue);
+        for (List<Integer> list : lists) {
+            int sum;
+            for (int i = 0; i < list.size(); i++) {
+                sum = list.get(i);
+                for (int j = i + 1; j < list.size(); j++) {
+                    sum += list.get(j);
+                    if (sum == targetSum) {
+                        result++;
+                        break;
+                    }
+                }
+            }
+        }
         return result;
     }
 
     private void iterate(TreeNode node, List<Integer> nodesValue) {
         if (node != null) {
             nodesValue.add(node.val);
+            List<Integer> rightNodesValue = nodesValue;
+            if (node.left != null && node.right != null) {
+                rightNodesValue = new ArrayList<>(nodesValue);
+                lists.add(rightNodesValue);
+            }
             iterate(node.left, nodesValue);
-            iterate(node.right, nodesValue);
+            if (node.right != null) {
+                iterate(node.right, rightNodesValue);
+            }
         }
     }
 }
