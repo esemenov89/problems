d3abc9c1fbbce026f5603072e773f1e5fcf91b7f
Evgeny Semenov <00semen00@gmail.com>
2025-03-18 17:10:19 +0300

    Solved problem 2185 from leetcode "Counting Words With a Given Prefix"
    https://leetcode.com/problems/counting-words-with-a-given-prefix/description/?envType=problem-list-v2&envId=string

--- src/main/java/org/just/leetcode/leetcode2185/Solution.java
+++ src/main/java/org/just/leetcode/leetcode2185/Solution.java
@@ -1,12 +1,14 @@
 package org.just.leetcode.leetcode2185;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Solution {
 
     public int prefixCount(String[] words, String pref) {
         int result = 0;
+        for (String word : words) {
+            if (word.startsWith(pref)) {
+                result++;
+            }
+        }
         return result;
     }
 }
--- src/test/java/org/just/leetcode/leetcode2185/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode2185/SolutionTest.java
@@ -25,7 +25,8 @@ public class SolutionTest {
         return Stream.of(
                 Arguments.of(new String[]{"pay","attention","practice","attend"}, "at", 2),
                 Arguments.of(new String[]{"leetcode","win","loops","success"}, "code", 0),
-                Arguments.of(new String[]{"a ".repeat(100), "b ".repeat(99)}, "a", 1)
+                Arguments.of(new String[]{"a ".repeat(100), "b ".repeat(99)}, "a", 1),
+                Arguments.of(new String[]{"a ".repeat(100) + "abc", "b ".repeat(99)}, "abc", 0)
         );
     }
 }
