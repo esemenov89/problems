1f9dd64fe32985fa6aa7b90e3c6554b85bbcbe47
Evgeny Semenov <00semen00@gmail.com>
2025-03-18 17:06:26 +0300

    Started problem 2185 from leetcode "Counting Words With a Given Prefix"
    https://leetcode.com/problems/counting-words-with-a-given-prefix/description/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode2185/Solution.java
@@ -0,0 +1,12 @@
+package org.just.leetcode.leetcode2185;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public int prefixCount(String[] words, String pref) {
+        int result = 0;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2185/SolutionTest.java
@@ -0,0 +1,31 @@
+package org.just.leetcode.leetcode2185;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String[] arg1, String arg2, int expected) {
+        int actual = solution.prefixCount(arg1, arg2);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(new String[]{"pay","attention","practice","attend"}, "at", 2),
+                Arguments.of(new String[]{"leetcode","win","loops","success"}, "code", 0),
+                Arguments.of(new String[]{"a ".repeat(100), "b ".repeat(99)}, "a", 1)
+        );
+    }
+}
