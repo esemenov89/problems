05f5408bb4b0f385c2dd50579a2af3575cd01039
Evgeny Semenov <00semen00@gmail.com>
2025-03-20 20:19:41 +0300

    Solved problem 709 from leetcode "To Lower Case"
    https://leetcode.com/problems/to-lower-case/description/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode709/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode709;
+
+public class Solution {
+
+    public String toLowerCase(String s) {
+        String result = s.toLowerCase();
+        return result;
+    }
+}
