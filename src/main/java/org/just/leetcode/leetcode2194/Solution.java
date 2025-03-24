16d986362ba0c12670cf4b8a61e3ee8a3c13587d
Evgeny Semenov <00semen00@gmail.com>
2025-03-24 19:37:43 +0300

    Solved problem 2194 from leetcode "Cells in a Range on an Excel Sheet"
    https://leetcode.com/problems/cells-in-a-range-on-an-excel-sheet/description/?envType=problem-list-v2&envId=string

--- src/main/java/org/just/leetcode/leetcode2194/Solution.java
+++ src/main/java/org/just/leetcode/leetcode2194/Solution.java
@@ -6,8 +6,27 @@ import java.util.List;
 
 public class Solution {
 
+    private final char[] ALPHABET = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
     public List<String> cellsInRange(String s) {
         List<String> result = new ArrayList<>();
+        boolean found = false;
+        for (char c : ALPHABET) {
+            if (s.charAt(0) == c || found) {
+                found = true;
+                result.add(c + s.substring(1, 2));
+                int start = Integer.parseInt(s.substring(1, 2));
+                int end = Integer.parseInt(s.substring(4, 5));
+                start++;
+                while (start <= end) {
+                    result.add(c + String.valueOf(start));
+                    start++;
+                }
+                if (c == s.charAt(3)) {
+                    break;
+                }
+            }
+        }
         return result;
     }
 }
--- src/test/java/org/just/leetcode/leetcode2194/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode2194/SolutionTest.java
@@ -16,7 +16,7 @@ public class SolutionTest {
 
     @ParameterizedTest
     @MethodSource("provideArguments")
-    void testSolution(String arg1, String expected) {
+    void testSolution(String arg1, List<String> expected) {
         List<String> actual = solution.cellsInRange(arg1);
 
         assertThat(actual, is(expected));
@@ -24,8 +24,11 @@ public class SolutionTest {
 
     private static Stream<Arguments> provideArguments() {
         return Stream.of(
-                Arguments.of("Hello", "hello"),
-                Arguments.of("here", "here"),
+                Arguments.of("K1:L2", List.of("K1", "K2", "L1", "L2")),
+                Arguments.of("A1:F1", List.of("A1", "B1", "C1", "D1", "E1", "F1")),
+                Arguments.of("A1:A9", List.of("A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9")),
+                Arguments.of("A2:A8", List.of("A2", "A3", "A4", "A5", "A6", "A7", "A8")),
+                Arguments.of("A2:B8", List.of("A2", "A3", "A4", "A5", "A6", "A7", "A8", "B2", "B3", "B4", "B5", "B6", "B7", "B8")),
                 Arguments.of("LOVELY", "lovely"),
                 Arguments.of("LOVELY", "lovely")
         );
