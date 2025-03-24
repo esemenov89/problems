675bde30b0e8945ff5fb9752b0adbe1560531ba0
Evgeny Semenov <00semen00@gmail.com>
2025-03-24 19:08:46 +0300

    Started problem 2194 from leetcode "Cells in a Range on an Excel Sheet"
    https://leetcode.com/problems/cells-in-a-range-on-an-excel-sheet/description/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode2194/Solution.java
@@ -0,0 +1,13 @@
+package org.just.leetcode.leetcode2194;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public List<String> cellsInRange(String s) {
+        List<String> result = new ArrayList<>();
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2194/SolutionTest.java
@@ -0,0 +1,33 @@
+package org.just.leetcode.leetcode2194;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String arg1, String expected) {
+        List<String> actual = solution.cellsInRange(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("Hello", "hello"),
+                Arguments.of("here", "here"),
+                Arguments.of("LOVELY", "lovely"),
+                Arguments.of("LOVELY", "lovely")
+        );
+    }
+}
