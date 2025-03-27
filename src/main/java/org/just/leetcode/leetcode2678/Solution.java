748bff896ed7f2071e54bfa065bae46c10dce7da
Evgeny Semenov <00semen00@gmail.com>
2025-03-27 20:35:25 +0300

    Started problem 2678 from leetcode "Number of Senior Citizens"
    https://leetcode.com/problems/number-of-senior-citizens/description/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode2678/Solution.java
@@ -0,0 +1,10 @@
+package org.just.leetcode.leetcode2678;
+
+
+public class Solution {
+
+    public int countSeniors(String[] details) {
+        int result = 0;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2678/SolutionTest.java
@@ -0,0 +1,32 @@
+package org.just.leetcode.leetcode2678;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String[] arg1, int expected) {
+        int actual = solution.countSeniors(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(new String[]{"7868190130M7522","5303914400F9211","9273338290F4010"}, 2),
+                Arguments.of(new String[]{"1313579440F2036","2921522980M5644"}, 0),
+                Arguments.of("a".repeat(1000), false),
+                Arguments.of("abcdefghijklmnopqrstuvwxyz".repeat(39), true)
+        );
+    }
+}
