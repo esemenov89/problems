3ac23cb77d87617b49bcf9d30fcb8bdb61b4e4b4
Evgeny Semenov <00semen00@gmail.com>
2025-03-29 17:49:34 +0300

    Continue problem 1372 from leetcode "Longest ZigZag Path in a Binary Tree"
    https://leetcode.com/problems/longest-zigzag-path-in-a-binary-tree/description/?envType=study-plan-v2&envId=leetcode-75

--- src/main/java/org/just/leetcode/study/plan/leetcode75/leetcode1372/Solution.java
+++ src/main/java/org/just/leetcode/study/plan/leetcode75/leetcode1372/Solution.java
@@ -1,21 +1,53 @@
 package org.just.leetcode.study.plan.leetcode75.leetcode1372;
 
-import java.util.List;
 
 public class Solution {
 
+    int maxZigZag = 0;
+    boolean isRoot = true;
+
     public int longestZigZag(TreeNode root) {
-        int result = 0;
+        int result;
+        traversePreOrder(root, 0, 0);
+        result = maxZigZag;
         return result;
     }
 
-    private void traversePreOrder(TreeNode node, List<Integer> list) {
+    private void traversePreOrder(TreeNode node, int count, int direction) {
         if (node != null) {
-            if (node.left != null || node.right != null) {
-                traversePreOrder(node.left, list);
-                traversePreOrder(node.right, list);
-            } else {
-                list.add(node.val);
+            if (node.left != null) {
+                if (direction == 1) {
+                    maxZigZag = Math.max(maxZigZag, count);
+                    traversePreOrder(node.left, 1, 0);
+                } else if (direction == 2 || direction == 0) {
+                    if (isRoot) {
+                        isRoot = false;
+                    } else {
+                        count++;
+                    }
+                    traversePreOrder(node.left, count, 1);
+                }
+            }
+            if (node.right != null) {
+                if (direction == 2) {
+                    maxZigZag = Math.max(maxZigZag, count);
+                    traversePreOrder(node.right, 1, 0);
+                } else if (direction == 1 || direction == 0) {
+                    if (isRoot) {
+                        isRoot = false;
+                    } else {
+                        count++;
+                    }
+                    traversePreOrder(node.right, count, 2);
+                }
+            }
+            if (node.left == null && node.right == null) {
+                if (isRoot) {
+                    isRoot = false;
+                } else {
+                    count++;
+                }
+                maxZigZag = Math.max(maxZigZag, count);
             }
         }
     }
--- src/test/java/org/just/leetcode/study/plan/leetcode75/leetcode1372/SolutionTest.java
+++ src/test/java/org/just/leetcode/study/plan/leetcode75/leetcode1372/SolutionTest.java
@@ -15,7 +15,7 @@ public class SolutionTest {
 
     @ParameterizedTest
     @MethodSource("provideArguments")
-    void leafSimilar(TreeNode arg1, int expected) {
+    void testSolution(TreeNode arg1, int expected) {
         int actual = solution.longestZigZag(arg1);
 
         assertThat(actual, is(expected));
@@ -25,63 +25,56 @@ public class SolutionTest {
         return Stream.of(
                 Arguments.of(generateTreeNode1(), 3),
                 Arguments.of(generateTreeNode2(), 4),
-                Arguments.of(new TreeNode(1), 0)
+                Arguments.of(new TreeNode(1), 0),
+                Arguments.of(generateTreeNode3(), 2),
+                Arguments.of(generateTreeNode4(), 1)
         );
     }
 
     private static TreeNode generateTreeNode1() {
-        TreeNode left = new TreeNode(9);
-        TreeNode rightLeft = new TreeNode(15);
-        TreeNode rightRight = new TreeNode(7);
-        TreeNode right = new TreeNode(20, rightLeft, rightRight);
-        return new TreeNode(3, left, right);
+        TreeNode rightLeft = new TreeNode(1);
+
+        TreeNode rightRightLeftRightRight = new TreeNode(1);
+        TreeNode rightRightLeftRight = new TreeNode(1, null, rightRightLeftRightRight);
+        TreeNode rightRightLeft = new TreeNode(1, null, rightRightLeftRight);
+
+        TreeNode rightRightRight = new TreeNode(1);
+
+        TreeNode rightRight = new TreeNode(1, rightRightLeft, rightRightRight);
+
+        TreeNode right = new TreeNode(1, rightLeft, rightRight);
+
+        return new TreeNode(1, null, right);
     }
 
     private static TreeNode generateTreeNode2() {
-        TreeNode leftRightLeft = new TreeNode(7);
-        TreeNode leftRightRight = new TreeNode(4);
-        TreeNode leftRight = new TreeNode(2, leftRightLeft, leftRightRight);
-        TreeNode leftLeft = new TreeNode(6);
-        TreeNode left = new TreeNode(5, leftLeft, leftRight);
+        TreeNode leftRightLeftRight = new TreeNode(1);
 
-        TreeNode rightLeft = new TreeNode(9);
-        TreeNode rightRight = new TreeNode(8);
-        TreeNode right = new TreeNode(1, rightLeft, rightRight);
-        return new TreeNode(3, left, right);
-    }
+        TreeNode leftRightLeft = new TreeNode(1, null, leftRightLeftRight);
+        TreeNode leftRightRight = new TreeNode(1);
 
-    private static TreeNode generateTreeNode22() {
-        TreeNode leftLeft = new TreeNode(6);
-        TreeNode leftRight = new TreeNode(7);
-        TreeNode left = new TreeNode(5, leftLeft, leftRight);
+        TreeNode leftRight = new TreeNode(1, leftRightLeft, leftRightRight);
+        TreeNode left = new TreeNode(1, null, leftRight);
 
-        TreeNode rightRightLeft = new TreeNode(9);
-        TreeNode rightRightRight = new TreeNode(8);
-        TreeNode rightLeft = new TreeNode(4);
-        TreeNode rightRight = new TreeNode(2, rightRightLeft, rightRightRight);
-        TreeNode right = new TreeNode(1, rightLeft, rightRight);
-        return new TreeNode(3, left, right);
-    }
+        TreeNode right = new TreeNode(1);
 
-    private static TreeNode generateTreeNode31() {
-        TreeNode left = new TreeNode(2);
-        TreeNode right = new TreeNode(3);
         return new TreeNode(1, left, right);
     }
 
-    private static TreeNode generateTreeNode32() {
-        TreeNode left = new TreeNode(3);
-        TreeNode right = new TreeNode(2);
-        return new TreeNode(1, left, right);
+    private static TreeNode generateTreeNode3() {
+        TreeNode rightRightRight = new TreeNode(1);
+        TreeNode rightRight = new TreeNode(1, null, rightRightRight);
+        TreeNode rightLeft = new TreeNode(1);
+
+        TreeNode right = new TreeNode(1, rightLeft, rightRight);
+
+        return new TreeNode(1, null, right);
     }
 
-    private static TreeNode generateTreeNode() {
-        TreeNode root = new TreeNode(1);
-        TreeNode last = root;
-        for (int i = 0; i < 200; i++) {
-            last.left = new TreeNode(200);
-            last = last.left;
-        }
-        return root;
+    private static TreeNode generateTreeNode4() {
+        TreeNode left = new TreeNode(1, new TreeNode(1), null);
+        TreeNode right = new TreeNode(1, null, new TreeNode(1));
+
+        return new TreeNode(1, left, right);
     }
 }
