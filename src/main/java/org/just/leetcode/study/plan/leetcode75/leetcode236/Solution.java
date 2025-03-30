f1d7db8314ae6ce5d2bb3fc168ab31e8eb297336
Evgeny Semenov <00semen00@gmail.com>
2025-03-30 16:37:55 +0300

    Solved problem 236 from leetcode "Lowest Common Ancestor of a Binary Tree"
    https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/?envType=study-plan-v2&envId=leetcode-75

--- src/main/java/org/just/leetcode/study/plan/leetcode75/leetcode236/Solution.java
+++ src/main/java/org/just/leetcode/study/plan/leetcode75/leetcode236/Solution.java
@@ -1,17 +1,69 @@
 package org.just.leetcode.study.plan.leetcode75.leetcode236;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
+
+    List<TreeNode> treeNodeOne = new ArrayList<>();
+    List<TreeNode> treeNodeTwo = new ArrayList<>();
+    boolean foundOne = false;
+    boolean foundTwo = false;
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         TreeNode result = null;
+        traversePreOrder(root, p.val, q.val, null, new ArrayList<>());
+        List<TreeNode> oneList = getAncestors(treeNodeOne);
+        List<TreeNode> twoList = getAncestors(treeNodeTwo);
+        for (TreeNode node : oneList) {
+            for (TreeNode treeNode : twoList) {
+                if (node == treeNode) {
+                    result = node;
+                    break;
+                }
+            }
+            if (result != null) {
+                break;
+            }
+        }
         return result;
     }
 
-    private void traversePreOrder(TreeNode node) {
+    private void traversePreOrder(TreeNode node, int val1, int val2, TreeNode ancestor, List<TreeNode> ancestors) {
         if (node != null) {
-            traversePreOrder(node.left);
-            traversePreOrder(node.right);
+            if (ancestor != null) {
+                if (ancestors.isEmpty() || ancestors.get(ancestors.size() - 1) != ancestor) {
+                    ancestors.add(ancestor);
+                }
+            }
+            if (node.val == val1) {
+                ancestors.add(node);
+                treeNodeOne = new ArrayList<>(ancestors);
+                foundOne = true;
+            }
+            if (node.val == val2) {
+                ancestors.add(node);
+                treeNodeTwo = new ArrayList<>(ancestors);
+                foundTwo = true;
+            }
+            if (!(foundOne && foundTwo)) {
+                traversePreOrder(node.left, val1, val2, node, ancestors);
+                traversePreOrder(node.right, val1, val2, node, ancestors);
+            }
         }
+    }
+
+    private List<TreeNode> getAncestors(List<TreeNode> list) {
+        List<TreeNode> result = new ArrayList<>();
+        TreeNode child = list.get(list.size() - 1);
+        result.add(child);
+        for (int i = list.size() - 2; i >= 0; i--) {
+            if (list.get(i).left == child || list.get(i).right == child) {
+                child = list.get(i);
+                result.add(child);
+            }
+        }
+        return result;
     }
 }
--- src/test/java/org/just/leetcode/study/plan/leetcode75/leetcode236/SolutionTest.java
+++ src/test/java/org/just/leetcode/study/plan/leetcode75/leetcode236/SolutionTest.java
@@ -18,14 +18,14 @@ public class SolutionTest {
     void testSolution(TreeNode arg1, TreeNode arg2, TreeNode arg3, TreeNode expected) {
         TreeNode actual = solution.lowestCommonAncestor(arg1, arg2, arg3);
 
-        assertThat(actual, is(expected));
+        assertThat(actual.val, is(expected.val));
     }
 
     private static Stream<Arguments> provideArguments() {
         return Stream.of(
-                Arguments.of(generateTreeNode1(), 3),
-                Arguments.of(generateTreeNode2(), 4),
-                Arguments.of(new TreeNode(1), 0),
+/*                Arguments.of(generateTreeNode1(), new TreeNode(5), new TreeNode(1), new TreeNode(3)),
+                Arguments.of(generateTreeNode2(), new TreeNode(5), new TreeNode(4), new TreeNode(5)),*/
+                Arguments.of(generateTreeNode3(), new TreeNode(1), new TreeNode(2), new TreeNode(1)),
                 Arguments.of(generateTreeNode3(), 2),
                 Arguments.of(generateTreeNode4(), 1),
                 Arguments.of(generateTreeNode5(), 3),
@@ -34,43 +34,25 @@ public class SolutionTest {
     }
 
     private static TreeNode generateTreeNode1() {
-        TreeNode rightLeft = new TreeNode(1);
+        TreeNode leftRight = new TreeNode(2, new TreeNode(7), new TreeNode(4));
 
-        TreeNode rightRightLeftRightRight = new TreeNode(1);
-        TreeNode rightRightLeftRight = new TreeNode(1, null, rightRightLeftRightRight);
-        TreeNode rightRightLeft = new TreeNode(1, null, rightRightLeftRight);
+        TreeNode left = new TreeNode(5, new TreeNode(6), leftRight);
+        TreeNode right = new TreeNode(1, new TreeNode(0), new TreeNode(8));
 
-        TreeNode rightRightRight = new TreeNode(1);
-
-        TreeNode rightRight = new TreeNode(1, rightRightLeft, rightRightRight);
-
-        TreeNode right = new TreeNode(1, rightLeft, rightRight);
-
-        return new TreeNode(1, null, right);
+        return new TreeNode(3, left, right);
     }
 
     private static TreeNode generateTreeNode2() {
-        TreeNode leftRightLeftRight = new TreeNode(1);
+        TreeNode leftRight = new TreeNode(2, new TreeNode(7), new TreeNode(4));
 
-        TreeNode leftRightLeft = new TreeNode(1, null, leftRightLeftRight);
-        TreeNode leftRightRight = new TreeNode(1);
+        TreeNode left = new TreeNode(5, new TreeNode(6), leftRight);
+        TreeNode right = new TreeNode(1, new TreeNode(0), new TreeNode(8));
 
-        TreeNode leftRight = new TreeNode(1, leftRightLeft, leftRightRight);
-        TreeNode left = new TreeNode(1, null, leftRight);
-
-        TreeNode right = new TreeNode(1);
-
-        return new TreeNode(1, left, right);
+        return new TreeNode(3, left, right);
     }
 
     private static TreeNode generateTreeNode3() {
-        TreeNode rightRightRight = new TreeNode(1);
-        TreeNode rightRight = new TreeNode(1, null, rightRightRight);
-        TreeNode rightLeft = new TreeNode(1);
-
-        TreeNode right = new TreeNode(1, rightLeft, rightRight);
-
-        return new TreeNode(1, null, right);
+        return new TreeNode(1, new TreeNode(2), null);
     }
 
     private static TreeNode generateTreeNode4() {
