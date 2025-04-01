6fe77c3ac01d6bceb24cf9ad605337f076daaab1
Evgeny Semenov <00semen00@gmail.com>
2025-04-01 20:55:03 +0300

    Solved problem 2810 from leetcode "Faulty Keyboard"
    https://leetcode.com/problems/faulty-keyboard/description/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode2810/Solution.java
@@ -0,0 +1,27 @@
+package org.just.leetcode.leetcode2810;
+
+
+public class Solution {
+
+    public String finalString(String s) {
+        String result;
+        String temp = "";
+        char tempChar;
+        char[] tempChars;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'i') {
+                tempChars = temp.toCharArray();
+                for (int j = 0, k = temp.length() - 1; j < temp.length() / 2 && k >= 0; j++, k--) {
+                    tempChar = tempChars[k];
+                    tempChars[k] = tempChars[j];
+                    tempChars[j] = tempChar;
+                }
+                temp = new String(tempChars);
+            } else {
+                temp += s.substring(i, i + 1);
+            }
+        }
+        result = temp;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2810/SolutionTest.java
@@ -0,0 +1,31 @@
+package org.just.leetcode.leetcode2810;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String arg1, String expected) {
+        String actual = solution.finalString(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("string", "rtsng"),
+                Arguments.of("poiinter", "ponter"),
+                Arguments.of("ab".repeat(100) + "i", "ba".repeat(100))
+        );
+    }
+}
