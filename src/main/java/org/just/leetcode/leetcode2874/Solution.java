ceed1fc4073260db70ba18f83d822500e3f69299
Evgeny Semenov <00semen00@gmail.com>
2025-04-03 16:13:49 +0300

    Started problem 2874 from leetcode "Maximum Value of an Ordered Triplet II"
    https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-ii/description/?envType=daily-question&envId=2025-04-03

---
+++ src/main/java/org/just/leetcode/leetcode2874/Solution.java
@@ -0,0 +1,19 @@
+package org.just.leetcode.leetcode2874;
+
+
+public class Solution {
+
+    public long maximumTripletValue(int[] nums) {
+        long result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if ((long) (nums[i] - nums[j]) * nums[k] > result) {
+                        result = (long) (nums[i] - nums[j]) * nums[k] ;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2874/SolutionTest.java
@@ -0,0 +1,41 @@
+package org.just.leetcode.leetcode2874;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(int[] arg1, long expected) {
+        long actual = solution.maximumTripletValue(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(new int[]{12, 6, 1, 2, 7}, 77),
+                Arguments.of(new int[]{1, 10, 3, 4, 19}, 133),
+                Arguments.of(new int[]{1, 2, 3}, 0),
+                Arguments.of(generateArray(), 999_999_000_000L)
+        );
+    }
+
+    private static int[] generateArray() {
+        int[] array = new int[100000];
+        Arrays.fill(array, 1_000_000);
+        array[0] = 1_000_000;
+        array[1] = 1;
+        return array;
+    }
+}
