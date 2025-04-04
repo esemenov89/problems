d7379523bcf8f4d3af1b3780b98956f1f6e8924b
Evgeny Semenov <00semen00@gmail.com>
2025-04-04 20:17:22 +0300

    Started problem 2315 from leetcode "Count Asterisks"
    https://leetcode.com/problems/count-asterisks/description/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode2315/Solution.java
@@ -0,0 +1,11 @@
+package org.just.leetcode.leetcode2315;
+
+
+public class Solution {
+
+    public int countAsterisks(String s) {
+        int result = 0;
+
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2315/SolutionTest.java
@@ -0,0 +1,31 @@
+package org.just.leetcode.leetcode2315;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String arg1, int expected) {
+        int actual = solution.countAsterisks(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("l|*e*et|c**o|*de|", 2),
+                Arguments.of("iamprogrammer", 0),
+                Arguments.of("yo|uar|e**|b|e***au|tifu|l", 5)
+        );
+    }
+}
