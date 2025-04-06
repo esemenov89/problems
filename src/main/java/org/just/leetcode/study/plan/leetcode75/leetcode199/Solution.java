323b6bfe0d8af6f764d06778e37651513682632b
Evgeny Semenov <00semen00@gmail.com>
2025-04-06 10:38:46 +0300

    Started problem 199 from leetcode "Binary Tree Right Side View"
    https://leetcode.com/problems/binary-tree-right-side-view/description/?envType=study-plan-v2&envId=leetcode-75

---
+++ src/main/java/org/just/leetcode/study/plan/leetcode75/leetcode199/Solution.java
@@ -0,0 +1,55 @@
+package org.just.leetcode.study.plan.leetcode75.leetcode199;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    List<TreeNode> treeNodeOne = new ArrayList<>();
+    List<TreeNode> treeNodeTwo = new ArrayList<>();
+    boolean foundOne = false;
+    boolean foundTwo = false;
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        return result;
+    }
+
+    private void traversePreOrder(TreeNode node, int val1, int val2, TreeNode ancestor, List<TreeNode> ancestors) {
+        if (node != null) {
+            if (ancestor != null) {
+                if (ancestors.isEmpty() || ancestors.get(ancestors.size() - 1) != ancestor) {
+                    ancestors.add(ancestor);
+                }
+            }
+            if (node.val == val1) {
+                ancestors.add(node);
+                treeNodeOne = new ArrayList<>(ancestors);
+                foundOne = true;
+            }
+            if (node.val == val2) {
+                ancestors.add(node);
+                treeNodeTwo = new ArrayList<>(ancestors);
+                foundTwo = true;
+            }
+            if (!(foundOne && foundTwo)) {
+                traversePreOrder(node.left, val1, val2, node, ancestors);
+                traversePreOrder(node.right, val1, val2, node, ancestors);
+            }
+        }
+    }
+
+    private List<TreeNode> getAncestors(List<TreeNode> list) {
+        List<TreeNode> result = new ArrayList<>();
+        TreeNode child = list.get(list.size() - 1);
+        result.add(child);
+        for (int i = list.size() - 2; i >= 0; i--) {
+            if (list.get(i).left == child || list.get(i).right == child) {
+                child = list.get(i);
+                result.add(child);
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/study/plan/leetcode75/leetcode199/SolutionTest.java
@@ -0,0 +1,84 @@
+package org.just.leetcode.study.plan.leetcode75.leetcode199;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(TreeNode arg1, List<Integer> expected) {
+        List<Integer> actual = solution.rightSideView(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(generateTreeNode1(), new TreeNode(5), new TreeNode(1), new TreeNode(3)),
+                Arguments.of(generateTreeNode2(), new TreeNode(5), new TreeNode(4), new TreeNode(5)),
+                Arguments.of(generateTreeNode3(), new TreeNode(1), new TreeNode(2), new TreeNode(1)),
+                Arguments.of(generateTreeNode3(), 2),
+                Arguments.of(generateTreeNode4(), 1),
+                Arguments.of(generateTreeNode5(), 3),
+                Arguments.of(generateTreeNode6(), 2)
+        );
+    }
+
+    private static TreeNode generateTreeNode1() {
+        TreeNode leftRight = new TreeNode(2, new TreeNode(7), new TreeNode(4));
+
+        TreeNode left = new TreeNode(5, new TreeNode(6), leftRight);
+        TreeNode right = new TreeNode(1, new TreeNode(0), new TreeNode(8));
+
+        return new TreeNode(3, left, right);
+    }
+
+    private static TreeNode generateTreeNode2() {
+        TreeNode leftRight = new TreeNode(2, new TreeNode(7), new TreeNode(4));
+
+        TreeNode left = new TreeNode(5, new TreeNode(6), leftRight);
+        TreeNode right = new TreeNode(1, new TreeNode(0), new TreeNode(8));
+
+        return new TreeNode(3, left, right);
+    }
+
+    private static TreeNode generateTreeNode3() {
+        return new TreeNode(1, new TreeNode(2), null);
+    }
+
+    private static TreeNode generateTreeNode4() {
+        TreeNode left = new TreeNode(1, new TreeNode(1), null);
+        TreeNode right = new TreeNode(1, null, new TreeNode(1));
+
+        return new TreeNode(1, left, right);
+    }
+
+    private static TreeNode generateTreeNode5() {
+        TreeNode leftRightLeft = new TreeNode(1, new TreeNode(1), null);
+        TreeNode leftRight = new TreeNode(1, leftRightLeft, new TreeNode(1));
+        TreeNode left = new TreeNode(1, null, leftRight);
+
+        return new TreeNode(1, left, null);
+    }
+
+    private static TreeNode generateTreeNode6() {
+        TreeNode rightRightRight = new TreeNode(1, new TreeNode(1), null);
+
+        TreeNode rightRight = new TreeNode(1, new TreeNode(1), rightRightRight);
+
+        TreeNode left = new TreeNode(1, new TreeNode(1), new TreeNode(1));
+        TreeNode right = new TreeNode(1, new TreeNode(1), rightRight);
+
+        return new TreeNode(1, null, right);
+    }
+}
