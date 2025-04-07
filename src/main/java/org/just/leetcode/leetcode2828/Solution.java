6c8b5251c1649691c66611ff2ddfc10d136662a7
Evgeny Semenov <00semen00@gmail.com>
2025-04-07 19:18:04 +0300

    Solved problem 2828 from leetcode "Check if a String Is an Acronym of Words"
    https://leetcode.com/problems/check-if-a-string-is-an-acronym-of-words/?envType=problem-list-v2&envId=string

--- src/main/java/org/just/leetcode/leetcode2828/Solution.java
+++ src/main/java/org/just/leetcode/leetcode2828/Solution.java
@@ -6,7 +6,16 @@ import java.util.List;
 public class Solution {
 
     public boolean isAcronym(List<String> words, String s) {
-        boolean result = false;
+        boolean result = true;
+        if (words.size() != s.length()) {
+            return false;
+        }
+        for (int i = 0; i < words.size(); i++) {
+            if (words.get(i).charAt(0) != s.charAt(i)) {
+                result = false;
+                break;
+            }
+        }
         return result;
     }
 }
--- src/test/java/org/just/leetcode/leetcode2828/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode2828/SolutionTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -16,7 +17,7 @@ public class SolutionTest {
 
     @ParameterizedTest
     @MethodSource("provideArguments")
-    void testSolution(List<String> arg1,String arg2, boolean expected) {
+    void testSolution(List<String> arg1, String arg2, boolean expected) {
         boolean actual = solution.isAcronym(arg1, arg2);
 
         assertThat(actual, is(expected));
@@ -24,9 +25,18 @@ public class SolutionTest {
 
     private static Stream<Arguments> provideArguments() {
         return Stream.of(
-                Arguments.of(List.of("alice","bob","charlie"), "abc", true),
-                Arguments.of(List.of("an","apple"), "a", false),
-                Arguments.of(List.of("never","gonna","give","up","on","you"), "ngguoy", true)
+                Arguments.of(List.of("alice", "bob", "charlie"), "abc", true),
+                Arguments.of(List.of("an", "apple"), "a", false),
+                Arguments.of(List.of("never", "gonna", "give", "up", "on", "you"), "ngguoy", true),
+                Arguments.of(generateWords(), "a".repeat(100), true)
         );
+    }
+
+    private static List<String> generateWords() {
+        List<String> words = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            words.add("ab");
+        }
+        return words;
     }
 }
