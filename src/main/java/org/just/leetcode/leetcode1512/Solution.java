28ce5fb86ef830a205695f2804a448ea11a339b3
Evgeny Semenov <00semen00@gmail.com>
2025-04-12 15:55:49 +0300

    Solved problem 1512 from leetcode "Number of Good Pairs"
    https://leetcode.com/problems/number-of-good-pairs/description/?envType=problem-list-v2&envId=hash-table

---
+++ src/main/java/org/just/leetcode/leetcode1512/Solution.java
@@ -0,0 +1,16 @@
+package org.just.leetcode.leetcode1512;
+
+public class Solution {
+
+    public int numIdenticalPairs(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+}
