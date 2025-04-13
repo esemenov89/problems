3d0db387aff780ab041662fa7d037d89474dda90
Evgeny Semenov <00semen00@gmail.com>
2025-04-13 14:30:51 +0300

    Started problem 1922 from leetcode "Count Good Numbers"
    https://leetcode.com/problems/count-good-numbers/description/?envType=daily-question&envId=2025-04-13

---
+++ src/main/java/org/just/leetcode/leetcode1922/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode1922;
+
+public class Solution {
+
+    public int countGoodNumbers(long n) {
+        int result = 0;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode1922/SolutionTest.java
@@ -0,0 +1,40 @@
+package org.just.leetcode.leetcode1922;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(long arg1, int expected) {
+        int actual = solution.countGoodNumbers(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(1, 5),
+                Arguments.of(4, 400),
+                Arguments.of(new int[]{1, 2, 3}, 0),
+                Arguments.of(new int[]{1, 2, 3, 1, 1, 3}, 4),
+                Arguments.of(generateArray(), 4950)
+        );
+    }
+
+    private static int[] generateArray() {
+        int[] array = new int[100];
+        Arrays.fill(array, 1);
+        return array;
+    }
+}
