2d7d884db1594487a1ca6eaf7917292cf1349f72
Evgeny Semenov <00semen00@gmail.com>
2025-04-17 12:20:06 +0300

    Solved problem 2176 from leetcode "Count Equal and Divisible Pairs in an Array"
    https://leetcode.com/problems/count-equal-and-divisible-pairs-in-an-array/description/?envType=daily-question&envId=2025-04-17

--- src/test/java/org/just/leetcode/leetcode2176/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode2176/SolutionTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class SolutionTest {
@@ -25,15 +26,13 @@ public class SolutionTest {
         return Stream.of(
                 Arguments.of(new int[]{3, 1, 2, 2, 2, 1, 3}, 2, 4),
                 Arguments.of(new int[]{1, 2, 3, 4}, 1, 0),
-                Arguments.of(generateArray(), 100, 100, 100, 161700)
+                Arguments.of(generateArray(), 1, 4950)
         );
     }
 
     private static int[] generateArray() {
         int[] array = new int[100];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = array.length - i;
-        }
+        Arrays.fill(array, 100);
         return array;
     }
 }
