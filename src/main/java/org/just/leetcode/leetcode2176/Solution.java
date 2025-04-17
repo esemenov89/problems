d8d0e0fdbd670625fb50c55254ef486297fe4907
Evgeny Semenov <00semen00@gmail.com>
2025-04-17 11:48:13 +0300

    Started problem 2176 from leetcode "Count Equal and Divisible Pairs in an Array"
    https://leetcode.com/problems/count-equal-and-divisible-pairs-in-an-array/description/?envType=daily-question&envId=2025-04-17

---
+++ src/main/java/org/just/leetcode/leetcode2176/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode2176;
+
+public class Solution {
+
+    public int countPairs(int[] nums, int k) {
+        int result = 0;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2176/SolutionTest.java
@@ -0,0 +1,39 @@
+package org.just.leetcode.leetcode2176;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(int[] arg1, int arg2, int expected) {
+        int actual = solution.countPairs(arg1, arg2);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(new int[]{3, 1, 2, 2, 2, 1, 3}, 2, 4),
+                Arguments.of(new int[]{1, 2, 3, 4}, 1, 0),
+                Arguments.of(generateArray(), 100, 100, 100, 161700)
+        );
+    }
+
+    private static int[] generateArray() {
+        int[] array = new int[100];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array.length - i;
+        }
+        return array;
+    }
+}
