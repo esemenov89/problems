10b12930212de9bcaf7c4785d001be2d34a86fe8
Evgeny Semenov <00semen00@gmail.com>
2025-04-19 14:27:10 +0300

    Started problem 1812 from leetcode "Determine Color of a Chessboard Square"
    https://leetcode.com/problems/determine-color-of-a-chessboard-square/description/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode1812/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode1812;
+
+public class Solution {
+
+    public boolean squareIsWhite(String coordinates) {
+        boolean result = false;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode1812/SolutionTest.java
@@ -0,0 +1,31 @@
+package org.just.leetcode.leetcode1812;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String arg1, boolean expected) {
+        boolean actual = solution.squareIsWhite(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("a1", false),
+                Arguments.of("h3", true),
+                Arguments.of("c7", false)
+        );
+    }
+}
