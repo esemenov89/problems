2ac97cacf38066dd929feaf7fc4857c4358b2101
Evgeny Semenov <00semen00@gmail.com>
2025-04-20 09:09:21 +0300

    Started problem 2710 from leetcode "Remove Trailing Zeros From a String"
    https://leetcode.com/problems/remove-trailing-zeros-from-a-string/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode2710/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode2710;
+
+public class Solution {
+
+    public String removeTrailingZeros(String num) {
+        String result = "";
+        return result;
+    }
+}
