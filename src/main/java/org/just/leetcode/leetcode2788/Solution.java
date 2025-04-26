60f6cb98ac81b73e71a20e6d51351cfd82eb2c41
Evgeny Semenov <00semen00@gmail.com>
2025-04-26 10:51:16 +0300

    Started problem 2788 from leetcode "Split Strings by Separator"
    https://leetcode.com/problems/split-strings-by-separator/description/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode2788/Solution.java
@@ -0,0 +1,12 @@
+package org.just.leetcode.leetcode2788;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public List<String> splitWordsBySeparator(List<String> words, char separator) {
+        List<String> result = new ArrayList<>();
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2788/SolutionTest.java
@@ -0,0 +1,33 @@
+package org.just.leetcode.leetcode2788;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.Collections.emptyList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(List<String> arg1, char arg2, List<String> expected) {
+        List<String> actual = solution.splitWordsBySeparator(arg1, arg2);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(List.of("one.two.three","four.five","six"), '.', List.of("one","two","three","four","five","six")),
+                Arguments.of(List.of("$easy$","$problem$"), '$', List.of("easy","problem")),
+                Arguments.of(List.of("|||"), '|', emptyList())
+        );
+    }
+}
