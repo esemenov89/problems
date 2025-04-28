79d970a291daf92b46d076f9b9b528669d752672
Evgeny Semenov <00semen00@gmail.com>
2025-04-28 14:15:56 +0300

    Started problem 1880 from leetcode "Check if Word Equals Summation of Two Words"
    https://leetcode.com/problems/check-if-word-equals-summation-of-two-words/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode1880/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode1880;
+
+public class Solution {
+
+    public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
+        boolean result = false;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode1880/SolutionTest.java
@@ -0,0 +1,31 @@
+package org.just.leetcode.leetcode1880;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String arg1, String arg2, String arg3, boolean expected) {
+        boolean actual = solution.isSumEqual(arg1, arg2, arg3);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("acb", "cba", "cdb", true),
+                Arguments.of("aaa", "a", "aab", false),
+                Arguments.of("aaa", "a", "aaaa", true)
+        );
+    }
+}
