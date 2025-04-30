d0d63a15cd19faf5a47b48a4250f4a1db44ab3ff
Evgeny Semenov <00semen00@gmail.com>
2025-04-30 14:21:14 +0300

    Solved problem 1295 from leetcode "Find Numbers with Even Number of Digits"
    https://leetcode.com/problems/find-numbers-with-even-number-of-digits/?envType=daily-question&envId=2025-04-30

--- src/main/java/org/just/leetcode/leetcode1295/Solution.java
+++ src/main/java/org/just/leetcode/leetcode1295/Solution.java
@@ -4,6 +4,11 @@ public class Solution {
 
     public int findNumbers(int[] nums) {
         int result = 0;
+        for (int num : nums) {
+            if (String.valueOf(num).length() % 2 == 0) {
+                result++;
+            }
+        }
         return result;
     }
 }
--- src/test/java/org/just/leetcode/leetcode1295/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode1295/SolutionTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class SolutionTest {
@@ -25,14 +24,20 @@ public class SolutionTest {
     private static Stream<Arguments> provideArguments() {
         return Stream.of(
                 Arguments.of(new int[]{12, 345, 2, 6, 7896}, 2),
-                Arguments.of(new int[]{12, 345, 2, 6, 7896}, 2),
-                Arguments.of(generateArray(), 1)
+                Arguments.of(new int[]{555, 901, 482, 1771}, 1),
+                Arguments.of(generateArray(), 250)
         );
     }
 
-    private static String[] generateArray() {
-        String[] words = new String[100];
-        Arrays.fill(words, "a".repeat(100));
-        return words;
+    private static int[] generateArray() {
+        int[] array = new int[500];
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                array[i] = 1;
+            } else {
+                array[i] = 10;
+            }
+        }
+        return array;
     }
 }
