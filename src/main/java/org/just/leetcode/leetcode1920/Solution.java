6ad910a54f278e650af487f1e7e50c875e69da0a
Evgeny Semenov <00semen00@gmail.com>
2025-05-06 19:32:10 +0300

    Started problem 1920 from leetcode "Build Array from Permutation"
    https://leetcode.com/problems/build-array-from-permutation/description/?envType=daily-question&envId=2025-05-06

---
+++ src/main/java/org/just/leetcode/leetcode1920/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode1920;
+
+public class Solution {
+
+    public int[] buildArray(int[] nums) {
+        int[] result = new int[nums.length];
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode1920/SolutionTest.java
@@ -0,0 +1,32 @@
+package org.just.leetcode.leetcode1920;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(int[] arg1, int[] expected) {
+        int[] actual = solution.buildArray(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(new int[]{0, 2, 1, 5, 3, 4}, new int[]{0, 1, 2, 4, 5, 3}),
+                Arguments.of(new int[]{5, 0, 1, 2, 3, 4}, new int[]{4, 5, 0, 1, 2, 3}),
+                Arguments.of(new int[]{5, 0, 1, 2, 3, 4}, new int[]{4, 5, 0, 1, 2, 3}),
+                Arguments.of(new int[]{5, 0, 1, 2, 3, 4}, new int[]{4, 5, 0, 1, 2, 3})
+        );
+    }
+}
