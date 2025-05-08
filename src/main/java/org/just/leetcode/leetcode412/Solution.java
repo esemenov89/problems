cbe42834d7171e34e8d2f51d8650c90789e65067
Evgeny Semenov <00semen00@gmail.com>
2025-05-08 14:04:02 +0300

    Started problem 412 from leetcode "Fizz Buzz"
    https://leetcode.com/problems/fizz-buzz/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode412/Solution.java
@@ -0,0 +1,12 @@
+package org.just.leetcode.leetcode412;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode412/SolutionTest.java
@@ -0,0 +1,32 @@
+package org.just.leetcode.leetcode412;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(int arg1, List<String> expected) {
+        List<String> actual = solution.fizzBuzz(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(3, List.of("1", "2", "Fizz")),
+                Arguments.of(5, List.of("1", "2", "Fizz", "4", "Buzz")),
+                Arguments.of(15, List.of("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"))
+        );
+    }
+}
