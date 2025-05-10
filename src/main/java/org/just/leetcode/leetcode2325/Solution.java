2028f8d720da428c1ba13f424a4ca07a1091055a
Evgeny Semenov <00semen00@gmail.com>
2025-05-10 09:46:17 +0300

    Started problem 2325 from leetcode "Decode the Message"
    https://leetcode.com/problems/decode-the-message/description/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode2325/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode2325;
+
+public class Solution {
+
+    public String decodeMessage(String key, String message) {
+        String result = "";
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2325/SolutionTest.java
@@ -0,0 +1,30 @@
+package org.just.leetcode.leetcode2325;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String arg1, String arg2, String expected) {
+        String actual = solution.decodeMessage(arg1, arg2);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("the quick brown fox jumps over the lazy dog", "vkbs bs t suepuv", "this is a secret"),
+                Arguments.of("eljuxhpwnyrdgtqkviszcfmabo", "zwx hnfx lqantp mnoeius ycgk vcnjrdb", "the five boxing wizards jump quickly")
+        );
+    }
+}
