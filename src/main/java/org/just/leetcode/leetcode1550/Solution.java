097c97cca2f7184c1479579c5b1e9618036de28b
Evgeny Semenov <00semen00@gmail.com>
2025-05-11 11:33:55 +0300

    Solved problem 1550 from leetcode "Three Consecutive Odds"
    https://leetcode.com/problems/three-consecutive-odds/description/?envType=daily-question&envId=2025-05-11

--- src/main/java/org/just/leetcode/leetcode1550/Solution.java
+++ src/main/java/org/just/leetcode/leetcode1550/Solution.java
@@ -4,6 +4,12 @@ public class Solution {
 
     public boolean threeConsecutiveOdds(int[] arr) {
         boolean result = false;
+        for (int i = 0; i < arr.length - 2; i++) {
+            if (arr[i] % 2 == 1 && arr[i + 1] % 2 == 1 && arr[i + 2] % 2 == 1) {
+                result = true;
+                break;
+            }
+        }
         return result;
     }
 }
--- src/test/java/org/just/leetcode/leetcode1550/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode1550/SolutionTest.java
@@ -24,7 +24,12 @@ public class SolutionTest {
     private static Stream<Arguments> provideArguments() {
         return Stream.of(
                 Arguments.of(new int[]{2, 6, 4, 1}, false),
-                Arguments.of(new int[]{1, 2, 34, 3, 4, 5, 7, 23, 12}, true)
+                Arguments.of(new int[]{1, 2, 34, 3, 4, 5, 7, 23, 12}, true),
+                Arguments.of(new int[]{2, 1, 3, 5}, true),
+                Arguments.of(new int[]{1, 1, 1}, true),
+                Arguments.of(new int[]{1}, false),
+                Arguments.of(generateArray(), false),
+                Arguments.of(generateArray2(), true)
         );
     }
 
@@ -32,6 +37,22 @@ public class SolutionTest {
         int[] result = new int[1000];
         for (int i = 0; i < result.length; i++) {
             result[i] = i;
+        }
+        return result;
+    }
+
+    private static int[] generateArray2() {
+        int[] result = new int[1000];
+        for (int i = 0; i < result.length; i++) {
+            if (i == 997) {
+                result[i] = 1;
+            } else if (i == 998) {
+                result[i] = 3;
+            } else if (i == 999) {
+                result[i] = 5;
+            } else {
+                result[i] = i;
+            }
         }
         return result;
     }
