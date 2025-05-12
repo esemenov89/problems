2f7457b1be2ef0d1dfc63b74f3ff6b93bb6924a2
Evgeny Semenov <00semen00@gmail.com>
2025-05-12 17:29:28 +0300

    Solved problem 2094 from leetcode "Finding 3-Digit Even Numbers"
    Решил, но очень неэффективным способом, если как-нибудь будет нечего делать, можно будет ускорить. На литкоде по времени чуть больше секунды алгоритм работал, что очень плохо) Сходу - надо брать сразу по три элемента, о либо еще лучше, смотреть какие цифры вообще есть в массиве и уже из них составлять, так как там очень много дублей может быть, цифр то всего 9 максимум) А элементов 100)
    
    https://leetcode.com/problems/finding-3-digit-even-numbers/?envType=daily-question&envId=2025-05-12

--- src/main/java/org/just/leetcode/leetcode2094/Solution.java
+++ src/main/java/org/just/leetcode/leetcode2094/Solution.java
@@ -1,9 +1,34 @@
 package org.just.leetcode.leetcode2094;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
 
     public int[] findEvenNumbers(int[] digits) {
         int[] result = new int[digits.length];
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] == 0) {
+                continue;
+            }
+            for (int j = 0; j < digits.length; j++) {
+                if (j == i) {
+                    continue;
+                }
+                for (int k = 0; k < digits.length; k++) {
+                    if (k == i || k == j) {
+                        continue;
+                    }
+                    if (digits[k] % 2 == 0) {
+                        set.add(Integer.valueOf(digits[i] + "" + digits[j] + digits[k]));
+                    }
+                }
+            }
+        }
+        result = set.stream().mapToInt(i -> i).toArray();
+        Arrays.sort(result);
         return result;
     }
 }
--- src/test/java/org/just/leetcode/leetcode2094/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode2094/SolutionTest.java
@@ -25,29 +25,18 @@ public class SolutionTest {
         return Stream.of(
                 Arguments.of(new int[]{2, 1, 3, 0}, new int[]{102, 120, 130, 132, 210, 230, 302, 310, 312, 320}),
                 Arguments.of(new int[]{2, 2, 8, 8, 2}, new int[]{222, 228, 282, 288, 822, 828, 882}),
-                Arguments.of(new int[]{3, 7, 5}, new int[]{})
+                Arguments.of(new int[]{3, 7, 5}, new int[]{}),
+                Arguments.of(generateArray(), new int[]{112, 122, 212, 222})
         );
     }
 
     private static int[] generateArray() {
-        int[] result = new int[1000];
+        int[] result = new int[100];
         for (int i = 0; i < result.length; i++) {
-            result[i] = i;
-        }
-        return result;
-    }
-
-    private static int[] generateArray2() {
-        int[] result = new int[1000];
-        for (int i = 0; i < result.length; i++) {
-            if (i == 997) {
+            if (i % 2 == 0) {
                 result[i] = 1;
-            } else if (i == 998) {
-                result[i] = 3;
-            } else if (i == 999) {
-                result[i] = 5;
             } else {
-                result[i] = i;
+                result[i] = 2;
             }
         }
         return result;
