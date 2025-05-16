7ef828ebf814c62ed098ce588f7cbd8b4623e563
Evgeny Semenov <00semen00@gmail.com>
2025-05-16 14:08:05 +0300

    Solved problem 2586 from leetcode "Count the Number of Vowel Strings in Range"
    https://leetcode.com/problems/count-the-number-of-vowel-strings-in-range/description/?envType=problem-list-v2&envId=string

--- src/main/java/org/just/leetcode/leetcode2586/Solution.java
+++ src/main/java/org/just/leetcode/leetcode2586/Solution.java
@@ -1,9 +1,18 @@
 package org.just.leetcode.leetcode2586;
 
+import java.util.Set;
+
 public class Solution {
+
+    private static final Set<Character> VOWELS = Set.of('a', 'e', 'i', 'o', 'u');
 
     public int vowelStrings(String[] words, int left, int right) {
         int result = 0;
+        for (int i = left; i <= right; i++) {
+            if (VOWELS.contains(words[i].charAt(0)) && VOWELS.contains(words[i].charAt(words[i].length() - 1))) {
+                result++;
+            }
+        }
         return result;
     }
 }
--- src/test/java/org/just/leetcode/leetcode2586/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode2586/SolutionTest.java
@@ -26,25 +26,14 @@ public class SolutionTest {
         return Stream.of(
                 Arguments.of(new String[]{"are", "amy", "u"}, 0, 2, 2),
                 Arguments.of(new String[]{"hey", "aeo", "mu", "ooo", "artro"}, 1, 4, 3),
-                Arguments.of(new String[]{"hey", "aeo", "mu", "ooo", "artro"}, 1, 4, 3)
+                Arguments.of(new String[]{"a"}, 0, 0, 1),
+                Arguments.of(generateStrings(), 0, 999, 1000)
         );
     }
 
-    private static int[] generateArray() {
-        int[] arr = new int[100];
-        for (int i = 0; i < arr.length; i++) {
-            if (i % 2 == 0) {
-                arr[i] = 0;
-            } else {
-                arr[i] = 1;
-            }
-        }
-        return arr;
-    }
-
     private static String[] generateStrings() {
-        String[] arr = new String[100];
-        Arrays.fill(arr, "a");
+        String[] arr = new String[1000];
+        Arrays.fill(arr, "aa");
         return arr;
     }
 }
