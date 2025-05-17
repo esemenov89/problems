31174a2a5217a43fadc1b7fbf5bc3dcec1b08220
Evgeny Semenov <00semen00@gmail.com>
2025-05-17 17:17:15 +0300

    Solved problem 2496 from leetcode "Maximum Value of a String in an Array"
    https://leetcode.com/problems/maximum-value-of-a-string-in-an-array/?envType=problem-list-v2&envId=string

--- src/main/java/org/just/leetcode/leetcode2496/Solution.java
+++ src/main/java/org/just/leetcode/leetcode2496/Solution.java
@@ -4,6 +4,17 @@ public class Solution {
 
     public int maximumValue(String[] strs) {
         int result = 0;
+        int temp;
+        for (String str : strs) {
+            try {
+                temp = Math.max(result, Integer.parseInt(str));
+            } catch(Exception e) {
+                temp = str.length();
+            }
+            if (temp > result) {
+                result = temp;
+            }
+        }
         return result;
     }
 }
--- src/test/java/org/just/leetcode/leetcode2496/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode2496/SolutionTest.java
@@ -25,13 +25,15 @@ public class SolutionTest {
     private static Stream<Arguments> provideArguments() {
         return Stream.of(
                 Arguments.of(new String[]{"alic3", "bob", "3", "4", "00000"}, 5),
-                Arguments.of(new String[]{"1", "01", "001", "0001"}, 1)
+                Arguments.of(new String[]{"1", "01", "001", "0001"}, 1),
+                Arguments.of(generateStrings(), 999_999_999)
         );
     }
 
     private static String[] generateStrings() {
         String[] arr = new String[1000];
-        Arrays.fill(arr, "aa");
+        Arrays.fill(arr, "a".repeat(9));
+        arr[0] = "999999999";
         return arr;
     }
 }
