79198f5a11a02a9df4a7a94a7ba1c92b8d1295b2
Evgeny Semenov <00semen00@gmail.com>
2025-05-17 17:00:02 +0300

    Started problem 2496 from leetcode "Maximum Value of a String in an Array"
    https://leetcode.com/problems/maximum-value-of-a-string-in-an-array/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode2496/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode2496;
+
+public class Solution {
+
+    public int maximumValue(String[] strs) {
+        int result = 0;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2496/SolutionTest.java
@@ -0,0 +1,37 @@
+package org.just.leetcode.leetcode2496;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String[] arg1, int expected) {
+        int actual = solution.maximumValue(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(new String[]{"alic3", "bob", "3", "4", "00000"}, 5),
+                Arguments.of(new String[]{"1", "01", "001", "0001"}, 1)
+        );
+    }
+
+    private static String[] generateStrings() {
+        String[] arr = new String[1000];
+        Arrays.fill(arr, "aa");
+        return arr;
+    }
+}
