b3f661a1db960d6e3d52686e4a5b9b46fc71c92e
Evgeny Semenov <00semen00@gmail.com>
2025-05-19 12:01:58 +0300

    Started problem 3024 from leetcode "Type of Triangle"
    https://leetcode.com/problems/type-of-triangle/description/?envType=daily-question&envId=2025-05-19

---
+++ src/main/java/org/just/leetcode/leetcode3024/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode3024;
+
+public class Solution {
+
+    public String triangleType(int[] nums) {
+        String result = "";
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode3024/SolutionTest.java
@@ -0,0 +1,37 @@
+package org.just.leetcode.leetcode3024;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(int[] arg1, String expected) {
+        String actual = solution.triangleType(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(new int[]{3, 3, 3}, "equilateral"),
+                Arguments.of(new int[]{3, 4, 5}, "scalene")
+        );
+    }
+
+    private static String[] generateStrings() {
+        String[] arr = new String[1000];
+        Arrays.fill(arr, "aa");
+        return arr;
+    }
+}
