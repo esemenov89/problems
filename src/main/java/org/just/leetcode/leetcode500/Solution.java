d8094a64e805424a9e244c38ae4b03b8d5fee934
Evgeny Semenov <00semen00@gmail.com>
2025-05-22 17:06:30 +0300

    Started problem 500 from leetcode "Keyboard Row"
    https://leetcode.com/problems/keyboard-row/description/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode500/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode500;
+
+public class Solution {
+
+    public String[] findWords(String[] words) {
+        String[] result = new String[words.length];
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode500/SolutionTest.java
@@ -0,0 +1,31 @@
+package org.just.leetcode.leetcode500;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String[] arg1, String[] expected) {
+        String[] actual = solution.findWords(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(new String[]{"Hello","Alaska","Dad","Peace"}, new String[]{"Alaska","Dad"}),
+                Arguments.of(new String[]{"omk"}, new String[]{}),
+                Arguments.of(new String[]{"adsdf","sfd"}, new String[]{"adsdf","sfd"})
+        );
+    }
+}
