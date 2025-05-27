519308c2b1fc56e6e8bc16b4371eb3ba1ca8e7a7
Evgeny Semenov <00semen00@gmail.com>
2025-05-27 10:11:49 +0300

    Started problem 2894 from leetcode "Divisible and Non-divisible Sums Difference"
    https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/?envType=daily-question&envId=2025-05-27

---
+++ src/main/java/org/just/leetcode/leetcode2894/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode2894;
+
+public class Solution {
+
+    public int differenceOfSums(int n, int m) {
+        int result = 0;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2894/SolutionTest.java
@@ -0,0 +1,31 @@
+package org.just.leetcode.leetcode2894;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(int arg1, int arg2, int expected) {
+        int actual = solution.differenceOfSums(arg1, arg2);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(10, 3, 19),
+                Arguments.of(5, 6, 15),
+                Arguments.of(5, 1, -15)
+        );
+    }
+}
