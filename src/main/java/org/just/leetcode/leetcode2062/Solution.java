2d30457849430b275979968946edd8ff9110c0aa
Evgeny Semenov <00semen00@gmail.com>
2025-05-30 14:12:33 +0300

    Started problem 2062 from leetcode "Count Vowel Substrings of a String"
    https://leetcode.com/problems/count-vowel-substrings-of-a-string/description/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode2062/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode2062;
+
+public class Solution {
+
+    public int countVowelSubstrings(String word) {
+        int result = 0;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2062/SolutionTest.java
@@ -0,0 +1,31 @@
+package org.just.leetcode.leetcode2062;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String arg1, int expected) {
+        int actual = solution.countVowelSubstrings(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("aeiouu", 2),
+                Arguments.of("unicornarihan", 0),
+                Arguments.of("cuaieuouac", 7)
+        );
+    }
+}
