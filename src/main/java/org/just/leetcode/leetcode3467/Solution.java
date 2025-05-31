09cd4dd50270c5d56fafde6e06116f3176a8ad5e
Evgeny Semenov <00semen00@gmail.com>
2025-05-31 14:26:54 +0300

    Solved problem 3467 from leetcode "Transform Array by Parity"
    https://leetcode.com/problems/transform-array-by-parity/description/?envType=problem-list-v2&envId=array

--- src/main/java/org/just/leetcode/leetcode3467/Solution.java
+++ src/main/java/org/just/leetcode/leetcode3467/Solution.java
@@ -1,9 +1,19 @@
 package org.just.leetcode.leetcode3467;
 
+import java.util.Arrays;
+
 public class Solution {
 
     public int[] transformArray(int[] nums) {
         int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                result[i] = 0;
+            } else {
+                result[i] = 1;
+            }
+        }
+        Arrays.sort(result);
         return result;
     }
 }
--- src/test/java/org/just/leetcode/leetcode3467/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode3467/SolutionTest.java
@@ -15,7 +15,7 @@ public class SolutionTest {
 
     @ParameterizedTest
     @MethodSource("provideArguments")
-    void testSolution(int[] arg1, int expected) {
+    void testSolution(int[] arg1, int[] expected) {
         int[] actual = solution.transformArray(arg1);
 
         assertThat(actual, is(expected));
@@ -23,8 +23,29 @@ public class SolutionTest {
 
     private static Stream<Arguments> provideArguments() {
         return Stream.of(
-                Arguments.of(new int[] {4,3,2,1}, new int[]{0,0,1,1}),
-                Arguments.of(new int[] {4,3,2,1}, new int[]{0,0,1,1})
+                Arguments.of(new int[]{4, 3, 2, 1}, new int[]{0, 0, 1, 1}),
+                Arguments.of(new int[]{4, 3, 2, 1}, new int[]{0, 0, 1, 1}),
+                Arguments.of(generateArray(), generateResultArray())
         );
+    }
+
+    private static int[] generateArray() {
+        int[] array = new int[100];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+        return array;
+    }
+
+    private static int[] generateResultArray() {
+        int[] array = new int[100];
+        for (int i = 0; i < array.length; i++) {
+            if (i < 50) {
+                array[i] = 0;
+            } else {
+                array[i] = 1;
+            }
+        }
+        return array;
     }
 }
