1b9ab2d685599d1f79e4d3df223bcc6819d1b9ec
Evgeny Semenov <00semen00@gmail.com>
2025-06-02 11:47:40 +0300

    Started problem 3190 from leetcode "Find Minimum Operations to Make All Elements Divisible by Three"
    https://leetcode.com/problems/find-minimum-operations-to-make-all-elements-divisible-by-three/?envType=problem-list-v2&envId=array

---
+++ src/main/java/org/just/leetcode/leetcode3190/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode3190;
+
+public class Solution {
+
+    public int minimumOperations(int[] nums) {
+        int result = 0;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode3190/SolutionTest.java
@@ -0,0 +1,43 @@
+package org.just.leetcode.leetcode3190;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(int[] arg1, int expected) {
+        int actual = solution.minimumOperations(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 3, 4}, 3),
+                Arguments.of(new int[]{3, 6, 9}, 0),
+                Arguments.of(new int[]{3, 6, 9}, 0)
+        );
+    }
+
+    private static int[] generateArray() {
+        int[] array = new int[1000];
+        for (int i = 0; i < array.length; i++) {
+            if (i < 500) {
+                array[i] = 1000;
+            } else {
+                array[i] = 999;
+            }
+        }
+        return array;
+    }
+}
