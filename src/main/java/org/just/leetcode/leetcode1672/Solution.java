78185af57d68515cfd0f88d3cc080662c15e5bce
Evgeny Semenov <00semen00@gmail.com>
2025-06-03 12:28:19 +0300

    Started problem 1672 from leetcode "Richest Customer Wealth"
    https://leetcode.com/problems/richest-customer-wealth/description/?envType=problem-list-v2&envId=array

---
+++ src/main/java/org/just/leetcode/leetcode1672/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode1672;
+
+public class Solution {
+
+    public int maximumWealth(int[][] accounts) {
+        int result = 0;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode1672/SolutionTest.java
@@ -0,0 +1,32 @@
+package org.just.leetcode.leetcode1672;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(int[][] arg1, int expected) {
+        int actual = solution.maximumWealth(arg1);
+
+        int[][] grid2 = new int[][]{{3, 1, 2, 2}, {1, 4, 4, 5}, {2, 4, 2, 2}, {2, 4, 2, 2}};
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(new int[][]{{1, 2, 3}, {3, 2, 1}}, 6),
+                Arguments.of(new int[][]{{1, 5}, {7, 3}, {3, 5}}, 10),
+                Arguments.of(new int[][]{{2, 8, 7}, {7, 1, 3}, {1, 9, 5}}, 17)
+        );
+    }
+}
