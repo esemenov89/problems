1eaef43019f5608b2f7225a47dc550d8f25023d2
Evgeny Semenov <00semen00@gmail.com>
2025-06-04 14:32:13 +0300

    mb solved problem 2824 from leetcode "Count Pairs Whose Sum is Less than Target"
    https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/?envType=problem-list-v2&envId=array

--- src/main/java/org/just/leetcode/leetcode2824/Solution.java
+++ src/main/java/org/just/leetcode/leetcode2824/Solution.java
@@ -6,6 +6,13 @@ public class Solution {
 
     public int countPairs(List<Integer> nums, int target) {
         int result = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums.get(i) + nums.get(j) < target) {
+                    result++;
+                }
+            }
+        }
         return result;
     }
 }
--- src/test/java/org/just/leetcode/leetcode2824/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode2824/SolutionTest.java
@@ -27,6 +27,6 @@ public class SolutionTest {
                 Arguments.of(List.of(-1, 1, 2, 3, 1), 2, 3),
                 Arguments.of(List.of(-6, 2, 5, -2, -7, -1, 3), -2, 10),
                 Arguments.of(List.of(-6, 2, 5, -2, -7, -1, 3), -2, 10)
-                );
+        );
     }
 }
