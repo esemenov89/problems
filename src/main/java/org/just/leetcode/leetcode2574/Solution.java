b952d42d9c95b4d3f4a87f3ed323adb81139bc02
Evgeny Semenov <00semen00@gmail.com>
2025-06-04 14:47:49 +0300

    Started problem 2574 from leetcode "Left and Right Sum Differences" https://leetcode.com/problems/left-and-right-sum-differences/?envType=problem-list-v2&envId=array

---
+++ src/main/java/org/just/leetcode/leetcode2574/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode2574;
+
+public class Solution {
+
+    public int[] leftRightDifference(int[] nums) {
+        int[] result = new int[nums.length];
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2574/SolutionTest.java
@@ -0,0 +1,31 @@
+package org.just.leetcode.leetcode2574;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(int[] arg1, int[] expected) {
+        int[] actual = solution.leftRightDifference(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(new int[]{10,4,8,3}, new int[]{15,1,11,22}),
+                Arguments.of(new int[]{1}, new int[]{0}),
+                Arguments.of(new int[]{10,4,8,3}, new int[]{10,4,8,3})
+        );
+    }
+}
