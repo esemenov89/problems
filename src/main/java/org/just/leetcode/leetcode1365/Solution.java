a2d79e0ced2825b2f868fac3ef940f37be0c7740
Evgeny Semenov <00semen00@gmail.com>
2025-06-05 12:23:24 +0300

    Started problem 1365 from leetcode "How Many Numbers Are Smaller Than the Current Number"
    https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/?envType=problem-list-v2&envId=array

---
+++ src/main/java/org/just/leetcode/leetcode1365/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode1365;
+
+public class Solution {
+
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] result = new int[nums.length];
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode1365/SolutionTest.java
@@ -0,0 +1,33 @@
+package org.just.leetcode.leetcode1365;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.just.leetcode.leetcode2574.Solution;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(int[] arg1, int[] expected) {
+        int[] actual = solution.leftRightDifference(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(new int[]{8, 1, 2, 2, 3}, new int[]{4, 0, 1, 1, 3}),
+                Arguments.of(new int[]{6, 5, 4, 8}, new int[]{2, 1, 0, 3}),
+                Arguments.of(new int[]{7, 7, 7, 7}, new int[]{0, 0, 0, 0}),
+                Arguments.of(new int[]{1000, 4, 8, 3}, new int[]{15, 989, 1001, 1012})
+        );
+    }
+}
