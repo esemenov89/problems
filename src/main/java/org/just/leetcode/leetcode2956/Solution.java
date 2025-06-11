799207b044973cb572a830768cc4a29b411b5172
Evgeny Semenov <00semen00@gmail.com>
2025-06-11 12:43:44 +0300

    Solved problem 2956 from leetcode "Find Common Elements Between Two Arrays"
    https://leetcode.com/problems/find-common-elements-between-two-arrays/description/?envType=problem-list-v2&envId=array

--- src/main/java/org/just/leetcode/leetcode2956/Solution.java
+++ src/main/java/org/just/leetcode/leetcode2956/Solution.java
@@ -4,6 +4,23 @@ public class Solution {
 
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
         int[] result = new int[2];
+        for (int k : nums1) {
+            for (int i : nums2) {
+                if (k == i) {
+                    result[0]++;
+                    break;
+                }
+            }
+        }
+        for (int k : nums2) {
+            for (int i : nums1) {
+                if (k == i) {
+                    result[1]++;
+                    break;
+                }
+            }
+        }
+
         return result;
     }
 }
