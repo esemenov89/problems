d49daf1d4cfdc573e62ccc6822c8dbbc630ed717
Evgeny Semenov <00semen00@gmail.com>
2025-06-12 20:24:34 +0300

    mb solved problem 3300 from leetcode "Minimum Element After Replacement With Digit Sum"
    https://leetcode.com/problems/minimum-element-after-replacement-with-digit-sum/description/?envType=problem-list-v2&envId=array

--- src/main/java/org/just/leetcode/leetcode3300/Solution.java
+++ src/main/java/org/just/leetcode/leetcode3300/Solution.java
@@ -3,7 +3,21 @@ package org.just.leetcode.leetcode3300;
 public class Solution {
 
     public int minElement(int[] nums) {
-        int result = 0;
+        int result = 100000;
+        for (int num : nums) {
+            if (sumDigits(num) < result) {
+                result = sumDigits(num);
+            }
+        }
         return result;
+    }
+
+    private int sumDigits(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n = n / 10;
+        }
+        return sum;
     }
 }
