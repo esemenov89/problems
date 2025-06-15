2dd03f9a7424406a589c481372f754f3c9c840ec
Evgeny Semenov <00semen00@gmail.com>
2025-06-15 13:16:07 +0300

    Started problem 3289 from leetcode "The Two Sneaky Numbers of Digitville"
    https://leetcode.com/problems/the-two-sneaky-numbers-of-digitville/description/?envType=problem-list-v2&envId=array

---
+++ src/main/java/org/just/leetcode/leetcode3289/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode3289;
+
+public class Solution {
+
+    public int[] getSneakyNumbers(int[] nums) {
+        int[] result = new int[2];
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode3289/SolutionTest.java
@@ -0,0 +1,35 @@
+package org.just.leetcode.leetcode3289;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(int[] arg1, int[] expected) {
+        int[] actual = solution.getSneakyNumbers(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(new int[]{0, 1, 1, 0}, new int[]{0, 1}),
+                Arguments.of(new int[]{0, 3, 2, 1, 3, 2}, new int[]{2, 3}),
+                Arguments.of(new int[]{7, 1, 5, 4, 3, 4, 6, 0, 9, 5, 8, 2}, new int[]{4, 5}),
+                Arguments.of(10, 90),
+                Arguments.of(90, 99),
+                Arguments.of(999, 999),
+                Arguments.of(100000000, 900000000)
+        );
+    }
+}
