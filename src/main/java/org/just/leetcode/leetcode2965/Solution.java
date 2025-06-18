11f0a45df0cc50803c9887991146743dbfdc29a8
Evgeny Semenov <00semen00@gmail.com>
2025-06-18 17:41:13 +0300

    Continue problem 2965 from leetcode "Find Missing and Repeated Values"
    https://leetcode.com/problems/find-missing-and-repeated-values/?envType=problem-list-v2&envId=array

--- src/main/java/org/just/leetcode/leetcode2965/Solution.java
+++ src/main/java/org/just/leetcode/leetcode2965/Solution.java
@@ -1,9 +1,34 @@
 package org.just.leetcode.leetcode2965;
 
+import java.util.Arrays;
+
 public class Solution {
 
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        int[] result = new int[grid.length * grid[0].length];
+        int[] result = new int[2];
+        int[] temp = new int[grid.length * grid[0].length];
+        int index = 0;
+        for (int[] arr : grid) {
+            for (int num : arr) {
+                temp[index++] = num;
+            }
+        }
+        Arrays.sort(temp);
+        index = 0;
+        for (int i = 1; i < temp.length; i++) {
+            if (temp[i] == temp[i - 1]) {
+                result[0] = temp[i];
+                index++;
+            } else {
+                if (temp[index] != i) {
+                    result[1] = i;
+                }
+            }
+            index++;
+            if (result[0] != 0 && result[1] != 0) {
+                break;
+            }
+        }
         return result;
     }
 }
