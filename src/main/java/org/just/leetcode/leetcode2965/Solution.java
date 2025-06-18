de400622a1e405165bb38f92ee55d95a260291a3
Evgeny Semenov <00semen00@gmail.com>
2025-06-18 16:55:02 +0300

    Started problem 2965 from leetcode "Find Missing and Repeated Values"
    https://leetcode.com/problems/find-missing-and-repeated-values/?envType=problem-list-v2&envId=array

---
+++ src/main/java/org/just/leetcode/leetcode2965/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode2965;
+
+public class Solution {
+
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        int[] result = new int[grid.length * grid[0].length];
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2965/SolutionTest.java
@@ -0,0 +1,31 @@
+package org.just.leetcode.leetcode2965;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(int[][] arg1, int[] expected) {
+        int[] actual = solution.findMissingAndRepeatedValues(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(new int[][]{{1, 3}, {2, 2}}, new int[]{2, 4}),
+                Arguments.of(new int[][]{{9, 1, 7}, {8, 9, 2}, {3, 4, 6}}, new int[]{9, 5}),
+                Arguments.of(new int[][]{{1, 3}, {2, 2}}, new int[]{2, 4})
+        );
+    }
+}
