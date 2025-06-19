e5d364f6947470bf4e32c4ed0e00a90117bbf1a4
Evgeny Semenov <00semen00@gmail.com>
2025-06-19 19:07:52 +0300

    Started problem 2294 from leetcode "Partition Array Such That Maximum Difference Is K"
    https://leetcode.com/problems/partition-array-such-that-maximum-difference-is-k/description/?envType=daily-question&envId=2025-06-19

---
+++ src/main/java/org/just/leetcode/leetcode2294/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode2294;
+
+public class Solution {
+
+    public int partitionArray(int[] nums, int k) {
+        int result = 0;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2294/SolutionTest.java
@@ -0,0 +1,32 @@
+package org.just.leetcode.leetcode2294;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(int[] arg1, int arg2, int expected) {
+        int actual = solution.partitionArray(arg1, arg2);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(new int[]{3, 6, 1, 2, 5}, 2, 2),
+                Arguments.of(new int[]{1, 2, 3}, 1, 2),
+                Arguments.of(new int[]{2, 2, 4, 5}, 0, 3),
+                Arguments.of(new int[]{3, 6, 1, 2, 5}, 2, 2)
+        );
+    }
+}
