3546a05ec7a6aeb0dbf14fc46eb759f11aadb2d0
Evgeny Semenov <00semen00@gmail.com>
2025-06-20 09:23:21 +0300

    Started problem 3162 from leetcode "Find the Number of Good Pairs I"
    https://leetcode.com/problems/find-the-number-of-good-pairs-i/description/?envType=problem-list-v2&envId=hash-table

---
+++ src/main/java/org/just/leetcode/leetcode3162/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode3162;
+
+public class Solution {
+
+    public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+        int result = 0;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode3162/SolutionTest.java
@@ -0,0 +1,44 @@
+package org.just.leetcode.leetcode3162;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(int[] arg1, int[] arg2, int arg3, int expected) {
+        int actual = solution.numberOfPairs(arg1, arg2, arg3);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 3, 4}, new int[]{1, 3, 4}, 1, 5),
+                Arguments.of(new int[]{1, 2, 4, 12}, new int[]{2, 4}, 3, 2),
+                Arguments.of(new int[]{1, 3, 4}, new int[]{1, 3, 4}, 1, 5)
+        );
+    }
+
+    private static int[] generateArray() {
+        int[] array = new int[100000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+        return array;
+    }
+
+    private static int[] generateArray2() {
+        int[] array = new int[100000];
+        return array;
+    }
+}
