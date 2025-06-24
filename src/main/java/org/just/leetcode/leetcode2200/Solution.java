f5b7b723727cee31744181272397226e142d2fa7
Evgeny Semenov <00semen00@gmail.com>
2025-06-24 14:33:33 +0300

    Started problem 2200 from leetcode "Find All K-Distant Indices in an Array"
    https://leetcode.com/problems/find-all-k-distant-indices-in-an-array/?envType=daily-question&envId=2025-06-24

---
+++ src/main/java/org/just/leetcode/leetcode2200/Solution.java
@@ -0,0 +1,12 @@
+package org.just.leetcode.leetcode2200;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+        List<Integer> result = new ArrayList<>();
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2200/SolutionTest.java
@@ -0,0 +1,32 @@
+package org.just.leetcode.leetcode2200;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(int[] arg1, int arg2, int arg3, List<Integer> expected) {
+        List<Integer> actual = solution.findKDistantIndices(arg1, arg2, arg3);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(new int[]{3, 4, 9, 1, 3, 9, 5}, 9, 1, List.of(1, 2, 3, 4, 5, 6)),
+                Arguments.of(new int[]{2, 2, 2, 2, 2}, 2, 2, List.of(0, 1, 2, 3, 4)),
+                Arguments.of(new int[]{3, 4, 9, 1, 3, 9, 5}, 9, 1, List.of(1, 2, 3, 4, 5, 6))
+        );
+    }
+}
