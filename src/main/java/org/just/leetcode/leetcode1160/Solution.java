656d0dfffd1cea3f264ff855549dd99fbff90f91
Evgeny Semenov <00semen00@gmail.com>
2025-06-25 16:41:41 +0300

    mb Solved problem 1160 from leetcode "Find Words That Can Be Formed by Characters"
    https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/?envType=problem-list-v2&envId=string

--- src/main/java/org/just/leetcode/leetcode1160/Solution.java
+++ src/main/java/org/just/leetcode/leetcode1160/Solution.java
@@ -4,6 +4,23 @@ public class Solution {
 
     public int countCharacters(String[] words, String chars) {
         int result = 0;
+        StringBuilder stringBuilder;
+        boolean isGoodWord;
+        for (String word : words) {
+            isGoodWord = true;
+            stringBuilder = new StringBuilder(chars);
+            for (int i = 0; i < word.length(); i++) {
+                if (stringBuilder.toString().contains(word.substring(i, i + 1))) {
+                    stringBuilder.deleteCharAt(stringBuilder.toString().indexOf(word.charAt(i)));
+                } else {
+                    isGoodWord = false;
+                    break;
+                }
+            }
+            if (isGoodWord) {
+                result += word.length();
+            }
+        }
         return result;
     }
 }
--- src/test/java/org/just/leetcode/leetcode1160/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode1160/SolutionTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class SolutionTest {
@@ -23,8 +24,17 @@ public class SolutionTest {
 
     private static Stream<Arguments> provideArguments() {
         return Stream.of(
-                Arguments.of(new String[]{"cat", "bt", "hat", "tree"}, "atach", 1),
-                Arguments.of(new String[]{"hello", "world", "leetcode"}, "welldonehoneyr", 1)
+                Arguments.of(new String[]{"cat", "bt", "hat", "tree"}, "atach", 6),
+                Arguments.of(new String[]{"hello", "world", "leetcode"}, "welldonehoneyr", 10),
+                Arguments.of(new String[]{"a"}, "a", 1),
+                Arguments.of(new String[]{"a"}, "b", 0),
+                Arguments.of(generateWords(), "a".repeat(100), 100000)
         );
+    }
+
+    private static String[] generateWords() {
+        String[] words = new String[1000];
+        Arrays.fill(words, "a".repeat(100));
+        return words;
     }
 }
