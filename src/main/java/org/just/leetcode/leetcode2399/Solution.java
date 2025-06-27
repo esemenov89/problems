eff6ae7ab2f2acdce8275b105227743b619ff2b9
Evgeny Semenov <00semen00@gmail.com>
2025-06-27 13:52:40 +0300

    Started problem 2399 from leetcode "Check Distances Between Same Letters"
    https://leetcode.com/problems/check-distances-between-same-letters/description/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode2399/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode2399;
+
+public class Solution {
+
+    public boolean checkDistances(String s, int[] distance) {
+        boolean result = true;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2399/SolutionTest.java
@@ -0,0 +1,31 @@
+package org.just.leetcode.leetcode2399;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String arg1, int[] arg2, boolean expected) {
+        boolean actual = solution.checkDistances(arg1, arg2);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("abaccb", new int[]{1, 3, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, true),
+                Arguments.of("aa", new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, false),
+                Arguments.of("abaccb", new int[]{1, 3, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, true)
+        );
+    }
+}
