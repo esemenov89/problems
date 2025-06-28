5477a1301a1b4b48a9eba6f355759d3096aa0efd
Evgeny Semenov <00semen00@gmail.com>
2025-06-28 14:28:45 +0300

    Solved problem 2099 from leetcode "Find Subsequence of Length K With the Largest Sum"
    https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/?envType=daily-question&envId=2025-06-28

--- src/main/java/org/just/leetcode/leetcode2099/Solution.java
+++ src/main/java/org/just/leetcode/leetcode2099/Solution.java
@@ -1,9 +1,32 @@
 package org.just.leetcode.leetcode2099;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
 
     public int[] maxSubsequence(int[] nums, int k) {
-        int[] result = new int[nums.length];
+        int[] result = new int[k];
+        Map<Integer, Integer> map = new HashMap<>();
+        int max;
+        int indexMax;
+        for (int i = 0; i < k; i++) {
+            max = -1000000;
+            indexMax = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] > max && !map.containsKey(j)) {
+                    max = nums[j];
+                    indexMax = j;
+                }
+            }
+            map.put(indexMax, max);
+        }
+        Integer[] temp = map.keySet().toArray(new Integer[0]);
+        Arrays.sort(temp);
+        for (int i = 0; i < temp.length; i++) {
+            result[i] = nums[temp[i]];
+        }
         return result;
     }
 }
--- src/test/java/org/just/leetcode/leetcode2099/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode2099/SolutionTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class SolutionTest {
@@ -24,16 +23,19 @@ public class SolutionTest {
 
     private static Stream<Arguments> provideArguments() {
         return Stream.of(
+                Arguments.of(new int[]{50, -75}, 2, new int[]{50, -75}),
                 Arguments.of(new int[]{2, 1, 3, 3}, 2, new int[]{3, 3}),
                 Arguments.of(new int[]{-1, -2, 3, 4}, 3, new int[]{-1, 3, 4}),
                 Arguments.of(new int[]{3, 4, 3, 3}, 2, new int[]{3, 4}),
-                Arguments.of(new int[]{3, 4, 3, 3}, 2, new int[]{3, 4})
+                Arguments.of(generateArray(), 1000, new int[]{3, 4})
         );
     }
 
-    private static String[] generateWords() {
-        String[] words = new String[1000];
-        Arrays.fill(words, "a".repeat(100));
-        return words;
+    private static int[] generateArray() {
+        int[] array = new int[1000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100000);
+        }
+        return array;
     }
 }
