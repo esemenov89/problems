cb61caabf50d6bbefec4a19828a4ee597f8da88e
Evgeny Semenov <00semen00@gmail.com>
2025-06-29 08:20:25 +0300

    Started problem 804 from leetcode "Number of Lines To Write String"
    https://leetcode.com/problems/number-of-lines-to-write-string/description/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode804/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode804;
+
+public class Solution {
+
+    public int[] numberOfLines(int[] widths, String s) {
+        int[] result = new int[widths.length];
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode804/SolutionTest.java
@@ -0,0 +1,41 @@
+package org.just.leetcode.leetcode804;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(int[] arg1, String arg2, int[] expected) {
+        int[] actual = solution.numberOfLines(arg1, arg2);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(new int[]{10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10}, "abcdefghijklmnopqrstuvwxyz", new int[]{3, 60}),
+                Arguments.of(new int[]{4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10}, "bbbcccdddaaa", new int[]{2, 4}),
+                Arguments.of(new int[]{10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10}, "abcdefghijklmnopqrstuvwxyz", new int[]{3, 60}),
+                Arguments.of(new int[]{10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10}, "abcdefghijklmnopqrstuvwxyz", new int[]{3, 60}),
+                Arguments.of(new int[]{10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10}, "abcdefghijklmnopqrstuvwxyz", new int[]{3, 60})
+        );
+    }
+
+    private static int[] generateArray() {
+        int[] array = new int[1000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100000);
+        }
+        return array;
+    }
+}
