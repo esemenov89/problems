21b959c79ab650351fb977709b19486b1890e16c
Evgeny Semenov <00semen00@gmail.com>
2025-06-30 18:33:12 +0300

    Solved problem 594 from leetcode "Longest Harmonious Subsequence"
    https://leetcode.com/problems/longest-harmonious-subsequence/?envType=daily-question&envId=2025-06-30

--- src/main/java/org/just/leetcode/leetcode594/Solution.java
+++ src/main/java/org/just/leetcode/leetcode594/Solution.java
@@ -1,59 +1,22 @@
 package org.just.leetcode.leetcode594;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Solution {
 
     public int findLHS(int[] nums) {
-        int result = 0;
-        int maxSubOne;
-        int minSubOne;
-        int maxSubTwo;
-        int minSubTwo;
-        int j;
-        List<Integer> subListOne;
-        List<Integer> subListTwo;
+        Arrays.sort(nums);
+        int j = 0;
+        int maxLength = 0;
+
         for (int i = 0; i < nums.length; i++) {
-            maxSubOne = nums[i];
-            minSubOne = nums[i];
-            maxSubTwo = nums[i];
-            minSubTwo = nums[i];
-            subListOne = new ArrayList<>();
-            subListTwo = new ArrayList<>();
-            subListOne.add(nums[i]);
-            subListTwo.add(nums[i]);
-            for (j = i + 1; j < nums.length; j++) {
-                if (nums[i] >= nums[j] && nums[i] - nums[j] <= 1) {
-                    subListOne.add(nums[j]);
-                    if (maxSubOne < nums[j]) {
-                        maxSubOne = nums[j];
-                    }
-                    if (minSubOne > nums[j]) {
-                        minSubOne = nums[j];
-                    }
-                }
-                if (nums[j] >= nums[i] && nums[j] - nums[i] <= 1) {
-                    subListTwo.add(nums[j]);
-                    if (maxSubTwo < nums[j]) {
-                        maxSubTwo = nums[j];
-                    }
-                    if (minSubTwo > nums[j]) {
-                        minSubTwo = nums[j];
-                    }
-                }
+            while (nums[i] - nums[j] > 1) {
+                j++;
             }
-            if (Math.abs(maxSubOne - minSubOne) == 1) {
-                if (subListOne.size() > result) {
-                    result = subListOne.size();
-                }
-            }
-            if (Math.abs(maxSubTwo - minSubTwo) == 1) {
-                if (subListTwo.size() > result) {
-                    result = subListTwo.size();
-                }
+            if (nums[i] - nums[j] == 1) {
+                maxLength = Math.max(maxLength, i - j + 1);
             }
         }
-        return result;
+        return maxLength;
     }
 }
