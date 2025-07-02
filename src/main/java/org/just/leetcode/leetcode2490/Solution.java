f70caa3d351245f2376f041bf965633863273cce
Evgeny Semenov <00semen00@gmail.com>
2025-07-02 15:34:46 +0300

    Started problem 2490 from leetcode "Circular Sentence"
    https://leetcode.com/problems/circular-sentence/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode2490/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode2490;
+
+public class Solution {
+
+    public boolean isCircularSentence(String sentence) {
+        boolean result = false;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2490/SolutionTest.java
@@ -0,0 +1,33 @@
+package org.just.leetcode.leetcode2490;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String arg1, boolean expected) {
+        boolean actual = solution.isCircularSentence(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("leetcode exercises sound delightful", true),
+                Arguments.of("eetcode", true),
+                Arguments.of("Leetcode is cool", false),
+                Arguments.of("a".repeat(100), true),
+                Arguments.of("a".repeat(50) + "b".repeat(50), false)
+        );
+    }
+}
