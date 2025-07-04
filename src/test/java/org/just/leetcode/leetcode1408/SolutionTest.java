f1151ec71ff2af658c0f8118c7498828f76e96e7
Evgeny Semenov <00semen00@gmail.com>
2025-07-04 14:21:00 +0300

    Solved problem 1408 from leetcode "String Matching in an Array"
    https://leetcode.com/problems/string-matching-in-an-array/description/?envType=problem-list-v2&envId=string

--- src/test/java/org/just/leetcode/leetcode1408/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode1408/SolutionTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -26,7 +27,15 @@ public class SolutionTest {
         return Stream.of(
                 Arguments.of(new String[]{"mass", "as", "hero", "superhero"}, List.of("as", "hero")),
                 Arguments.of(new String[]{"leetcode", "et", "code"}, List.of("et", "code")),
-                Arguments.of(new String[]{"blue", "green", "bu"}, List.of())
+                Arguments.of(new String[]{"blue", "green", "bu"}, List.of()),
+                Arguments.of(new String[]{"leetcoder", "leetcode", "od", "hamlet", "am"}, List.of("leetcode", "od", "am")),
+                Arguments.of(generateStrings(), List.of())
         );
+    }
+
+    private static String[] generateStrings() {
+        String[] strings = new String[100];
+        Arrays.fill(strings, "a");
+        return strings;
     }
 }
