472f64647f2ff9b18a94a16f92c01dd7916726fb
Evgeny Semenov <00semen00@gmail.com>
2025-07-06 03:39:47 +0300

    Started problem 3232 from leetcode "Find if Digit Game Can Be Won"
    https://leetcode.com/problems/find-if-digit-game-can-be-won/?envType=problem-list-v2&envId=array

---
+++ src/main/java/org/just/leetcode/leetcode3232/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode3232;
+
+public class Solution {
+
+    public boolean canAliceWin(int[] nums) {
+        boolean result = false;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode3232/SolutionTest.java
@@ -0,0 +1,45 @@
+package org.just.leetcode.leetcode3232;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(int[] arg1, boolean expected) {
+        boolean actual = solution.canAliceWin(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 3, 4, 10}, false),
+                Arguments.of(new int[]{1, 2, 3, 4, 5, 14}, true),
+                Arguments.of(new int[]{5, 5, 5, 25}, true),
+                Arguments.of(new int[]{2, 2, 3, 4}, 2),
+                Arguments.of(generateArray(), 250)
+        );
+    }
+
+    private static int[] generateArray() {
+        int[] array = new int[500];
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                array[i] = 250;
+            } else {
+                array[i] = 10;
+            }
+        }
+        return array;
+    }
+}
