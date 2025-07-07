5e5f0d9417986636e3ea45e45323d233ab2eb50a
Evgeny Semenov <00semen00@gmail.com>
2025-07-07 11:16:35 +0300

    Started problem 824 from leetcode "Goat Latin"
    https://leetcode.com/problems/goat-latin/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode824/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode824;
+
+public class Solution {
+
+    public String toGoatLatin(String sentence) {
+        String result = "";
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode824/SolutionTest.java
@@ -0,0 +1,32 @@
+package org.just.leetcode.leetcode824;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String arg1, String expected) {
+        String actual = solution.toGoatLatin(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("I speak Goat Latin", "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"),
+                Arguments.of("The quick brown fox jumped over the lazy dog", "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"),
+                Arguments.of("I speak Goat Latin", "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"),
+                Arguments.of("I speak Goat Latin", "Imaa peaksmaaa oatGmaaaa atinLmaaaaa")
+        );
+    }
+}
