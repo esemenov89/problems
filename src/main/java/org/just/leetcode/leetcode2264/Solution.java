9a84e46d04eeb1344cc3dee42b296800cfe8cf90
Evgeny Semenov <00semen00@gmail.com>
2025-07-08 13:43:25 +0300

    Started problem 2264 from leetcode "Largest 3-Same-Digit Number in String"
    https://leetcode.com/problems/largest-3-same-digit-number-in-string/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode2264/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode2264;
+
+public class Solution {
+
+    public String largestGoodInteger(String num) {
+        String result = "";
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2264/SolutionTest.java
@@ -0,0 +1,32 @@
+package org.just.leetcode.leetcode2264;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String arg1, String expected) {
+        String actual = solution.largestGoodInteger(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("6777133339", "777"),
+                Arguments.of("2300019", "000"),
+                Arguments.of("42352338", ""),
+                Arguments.of("6777133339", "777")
+        );
+    }
+}
