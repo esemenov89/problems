e3e66857ca9a37dc8adc03b21590944792d5f988
Evgeny Semenov <00semen00@gmail.com>
2025-07-12 10:46:00 +0300

    Solved problem 1694 from leetcode "Reformat Phone Number"
    https://leetcode.com/problems/reformat-phone-number/?envType=problem-list-v2&envId=string

--- src/main/java/org/just/leetcode/leetcode1694/Solution.java
+++ src/main/java/org/just/leetcode/leetcode1694/Solution.java
@@ -3,7 +3,30 @@ package org.just.leetcode.leetcode1694;
 public class Solution {
 
     public String reformatNumber(String number) {
-        String result = "";
+        String result;
+        result = number.replaceAll("[- ]", "");
+        if (result.length() == 2 || result.length() == 3) {
+        } else if (result.length() == 4) {
+            result = result.substring(0, 2) + "-" + result.substring(2);
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < result.length() - 2; i += 3) {
+                if (result.length() - i == 4) {
+                    sb.append(result, i, i + 2).append("-").append(result.substring(i + 2));
+                    break;
+                }
+                sb.append(result, i, i + 3);
+                if (i + 3 < result.length()) {
+                    sb.append("-");
+                }
+                if (i + 3 >= result.length() - 2) {
+                    sb.append(result, i + 3, result.length());
+                    break;
+                }
+            }
+            result = sb.toString();
+        }
+
         return result;
     }
 }
--- src/test/java/org/just/leetcode/leetcode1694/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode1694/SolutionTest.java
@@ -23,10 +23,16 @@ public class SolutionTest {
 
     private static Stream<Arguments> provideArguments() {
         return Stream.of(
+                Arguments.of("12345", "123-45"),
                 Arguments.of("1-23-45 6", "123-456"),
                 Arguments.of("123 4-567", "123-45-67"),
                 Arguments.of("123 4-5678", "123-456-78"),
-                Arguments.of("1-23-45 6", "123-456")
+                Arguments.of("123 4-56789", "123-456-789"),
+                Arguments.of("123 4-567890", "123-456-78-90"),
+                Arguments.of("12", "12"),
+                Arguments.of("123", "123"),
+                Arguments.of("123".repeat(30) + "12", "123-123-123-123-123-123-123-123-123-123-123-123-123-123-123-123-123-123-123-123-123-123-123-123-123-123-123-123-123-123-12"),
+                Arguments.of("1234", "12-34")
         );
     }
 }
