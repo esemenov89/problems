d0b94302cb3814fd3a2597ee5f40de6234193c7b
Evgeny Semenov <00semen00@gmail.com>
2025-07-14 14:56:40 +0300

    Started problem 1290 from leetcode "Convert Binary Number in a Linked List to Integer"
    https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/description/?envType=daily-question&envId=2025-07-14

---
+++ src/main/java/org/just/leetcode/leetcode1290/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode1290;
+
+public class Solution {
+
+    public int getDecimalValue(ListNode head) {
+        int result = 0;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode1290/SolutionTest.java
@@ -0,0 +1,47 @@
+package org.just.leetcode.leetcode1290;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(ListNode arg1, int expected) {
+        int actual = solution.getDecimalValue(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(generateListNode1(), 5),
+                Arguments.of(new ListNode(0), 0),
+                Arguments.of(generateListNode2(), 5)
+        );
+    }
+
+
+    private static ListNode generateListNode1() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(0);
+        head.next.next = new ListNode(1);
+        return head;
+    }
+
+    private static ListNode generateListNode2() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(0);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(1);
+        return head;
+    }
+}
