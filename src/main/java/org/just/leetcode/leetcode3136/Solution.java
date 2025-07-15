282467dfa67cbfcd34f4ba350792d5a13e472141
Evgeny Semenov <00semen00@gmail.com>
2025-07-15 13:58:50 +0300

    Started problem 3136 from leetcode "Valid Word"
    https://leetcode.com/problems/valid-word/?envType=daily-question&envId=2025-07-15

---
+++ src/main/java/org/just/leetcode/leetcode3136/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode3136;
+
+public class Solution {
+
+    public boolean isValid(String word) {
+        boolean result = false;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode3136/SolutionTest.java
@@ -0,0 +1,33 @@
+package org.just.leetcode.leetcode3136;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String arg1, boolean expected) {
+        boolean actual = solution.isValid(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("234Adas", true),
+                Arguments.of("234Adas", true),
+                Arguments.of("234Adas", true),
+                Arguments.of("234Adas", true),
+                Arguments.of("234Adas", true)
+        );
+    }
+}
