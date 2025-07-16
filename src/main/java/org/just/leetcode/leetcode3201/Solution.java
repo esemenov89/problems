3c7d306a025bf6d8612cd10fcd67e4ef5331533e
Evgeny Semenov <00semen00@gmail.com>
2025-07-16 16:18:19 +0300

    Solved problem 3201 from leetcode "Find the Maximum Length of Valid Subsequence I"
    https://leetcode.com/problems/find-the-maximum-length-of-valid-subsequence-i/?envType=daily-question&envId=2025-07-16

--- src/main/java/org/just/leetcode/leetcode3201/Solution.java
+++ src/main/java/org/just/leetcode/leetcode3201/Solution.java
@@ -3,7 +3,59 @@ package org.just.leetcode.leetcode3201;
 public class Solution {
 
     public int maximumLength(int[] nums) {
-        int result = 0;
+        int result;
+        int[] zeroFirst = new int[2];
+        int[] oneFirst = new int[2];
+        int[] zeroNotFirst = new int[2];
+        int[] oneNotFirst = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                zeroFirst[0] = nums[i];
+                oneFirst[0] = nums[i];
+                zeroFirst[1] = 1;
+                oneFirst[1] = 1;
+            } else {
+                if ((zeroFirst[0] + nums[i]) % 2 == 0) {
+                    zeroFirst[1]++;
+                    zeroFirst[0] = nums[i];
+                }
+                if ((oneFirst[0] + nums[i]) % 2 == 1) {
+                    oneFirst[1]++;
+                    oneFirst[0] = nums[i];
+                }
+                if (zeroNotFirst[0] != 0) {
+                    if ((zeroNotFirst[0] + nums[i]) % 2 == 0) {
+                        zeroNotFirst[1]++;
+                        zeroNotFirst[0] = nums[i];
+                    }
+                    if ((oneFirst[0] + nums[i]) % 2 == 1) {
+                        oneNotFirst[1]++;
+                        oneNotFirst[0] = nums[i];
+                    }
+                }
+                if (zeroNotFirst[0] == 0 && nums[0] % 2 != nums[i] % 2) {
+                    zeroNotFirst[0] = nums[i];
+                    oneNotFirst[0] = nums[i];
+                    zeroNotFirst[1] = 1;
+                    oneNotFirst[1] = 1;
+                }
+            }
+        }
+        result = Math.max(Math.max(zeroFirst[1], oneFirst[1]), Math.max(zeroNotFirst[1], oneNotFirst[1]));
         return result;
     }
+
+/*            for (int i = 0; i < nums.length - 1; i++) {
+        Integer temp = map.get((nums[i] + nums[i + 1]) % 2);
+        if (temp != null) {
+            map.put((nums[i] + nums[i + 1]) % 2, ++temp);
+        } else {
+            map.put((nums[i] + nums[i + 1]) % 2, 2);
+        }
+    }
+        for (Integer value : map.values()) {
+        if (value > result) {
+            result = value;
+        }
+    }*/
 }
--- src/test/java/org/just/leetcode/leetcode3201/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode3201/SolutionTest.java
@@ -23,9 +23,33 @@ public class SolutionTest {
 
     private static Stream<Arguments> provideArguments() {
         return Stream.of(
+                Arguments.of(new int[]{1, 8, 4, 2, 4}, 4),
+                Arguments.of(new int[]{1, 5, 9, 4, 2}, 3),
                 Arguments.of(new int[]{1, 2, 3, 4}, 4),
                 Arguments.of(new int[]{1, 2, 1, 1, 2, 1, 2}, 6),
-                Arguments.of(new int[]{1, 3}, 2)
+                Arguments.of(new int[]{1, 3}, 2),
+                Arguments.of(generateArray(), 20000),
+                Arguments.of(generateArray2(), 20000)
         );
+    }
+
+    private static int[] generateArray() {
+        int[] array = new int[20000];
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                array[i] = 1;
+            } else {
+                array[i] = 2;
+            }
+        }
+        return array;
+    }
+
+    private static int[] generateArray2() {
+        int[] array = new int[20000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+        return array;
     }
 }
