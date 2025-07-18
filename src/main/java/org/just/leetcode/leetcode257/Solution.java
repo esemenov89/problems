c4a2b5741353e9ef86f56aa4c082a05144a3736b
Evgeny Semenov <00semen00@gmail.com>
2025-07-18 11:54:07 +0300

    Solved problem 257 from leetcode "Binary Tree Paths"
    https://leetcode.com/problems/binary-tree-paths/?envType=problem-list-v2&envId=string

--- src/main/java/org/just/leetcode/leetcode257/Solution.java
+++ src/main/java/org/just/leetcode/leetcode257/Solution.java
@@ -5,8 +5,27 @@ import java.util.List;
 
 public class Solution {
 
+    private final List<String> temp = new ArrayList<>();
+    private boolean isNotRoot = false;
+
     public List<String> binaryTreePaths(TreeNode root) {
-        List<String> result = new ArrayList<>();
+        List<String> result;
+        traversePreOrder(root, new StringBuilder("" + root.val));
+        result = temp;
         return result;
+    }
+
+    private void traversePreOrder(TreeNode node, StringBuilder sb) {
+        if (node != null) {
+            if (isNotRoot) {
+                sb.append("->").append(node.val);
+            }
+            isNotRoot = true;
+            if (node.left == null && node.right == null) {
+                temp.add(sb.toString());
+            }
+            traversePreOrder(node.left, new StringBuilder(sb));
+            traversePreOrder(node.right, new StringBuilder(sb));
+        }
     }
 }
--- src/test/java/org/just/leetcode/leetcode257/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode257/SolutionTest.java
@@ -26,10 +26,10 @@ public class SolutionTest {
         return Stream.of(
                 Arguments.of(generateTreeNode1(), List.of("1->2->5","1->3")),
                 Arguments.of(new TreeNode(1), List.of("1")),
-                Arguments.of(generateTreeNode2(), List.of("1")),
-                Arguments.of(new TreeNode(1), List.of("1->2->5","1->3")),
-                Arguments.of(generateTreeNode(), List.of("1->2->5","1->3")),
-                Arguments.of(generateTreeNode3(), List.of("1->2->5","1->3"))
+                Arguments.of(generateTreeNode2(), List.of("1->2")),
+                Arguments.of(generateTreeNode(), List.of("1->2->3->4->5->6->7->8->9->10->11->12->13->14->15->16->17->18->19->20->21->22->23->24->25->26->27->28->29->30->31->32->33->34->35->36->37->38->39->40->41->42->43->44->45->46->47->48->49->50->51->52->53->54->55->56->57->58->59->60->61->62->63->64->65->66->67->68->69->70->71->72->73->74->75->76->77->78->79->80->81->82->83->84->85->86->87->88->89->90->91->92->93->94->95->96->97->98->99->100->101")),
+                Arguments.of(generateTreeNode3(), List.of("1->2->4", "1->3->5")),
+                Arguments.of(generateTreeNode4(), List.of("1->2->3", "1->2->4", "1->20->30", "1->20->40"))
         );
     }
 
@@ -53,10 +53,16 @@ public class SolutionTest {
         return new TreeNode(1, left, right);
     }
 
+    private static TreeNode generateTreeNode4() {
+        TreeNode left = new TreeNode(2, new TreeNode(3), new TreeNode(4));
+        TreeNode right = new TreeNode(20, new TreeNode(30), new TreeNode(40));
+        return new TreeNode(1, left, right);
+    }
+
     private static TreeNode generateTreeNode() {
         TreeNode root = new TreeNode(1);
         TreeNode last = root;
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 100; i++) {
             last.left = new TreeNode(i + 2);
             last = last.left;
         }
