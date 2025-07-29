c3bb3f69489148b5475a19089124d87ff5d0d60e
Evgeny Semenov <00semen00@gmail.com>
2025-07-29 12:53:33 +0300

    Started problem 2192 from leetcode "Capitalize the Title"
    https://leetcode.com/problems/capitalize-the-title/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode2192/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode2192;
+
+public class Solution {
+
+    public String capitalizeTitle(String title) {
+        String result = "";
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2192/SolutionTest.java
@@ -0,0 +1,31 @@
+package org.just.leetcode.leetcode2192;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String arg1, String expected) {
+        String actual = solution.capitalizeTitle(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("capiTalIze tHe titLe", "Capitalize The Title"),
+                Arguments.of("First leTTeR of EACH Word", "First Letter of Each Word"),
+                Arguments.of("i lOve leetcode", "i Love Leetcode")
+        );
+    }
+}
