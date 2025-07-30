18c4e0771be0e0c9a8fb2de41b13289405fbcd5d
Evgeny Semenov <00semen00@gmail.com>
2025-07-30 17:15:02 +0300

    Started problem 2243 from leetcode "Calculate Digit Sum of a String"
    https://leetcode.com/problems/calculate-digit-sum-of-a-string/description/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode2243/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode2243;
+
+public class Solution {
+
+    public String digitSum(String s, int k) {
+        String result = "";
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2243/SolutionTest.java
@@ -0,0 +1,32 @@
+package org.just.leetcode.leetcode2243;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String arg1, int arg2, String expected) {
+        String actual = solution.digitSum(arg1, arg2);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("11111222223", 3, "135"),
+                Arguments.of("00000000", 3, "0000"),
+                Arguments.of("00000000", 3, "0000"),
+                Arguments.of("00000000", 3, "0000")
+        );
+    }
+}
