aee7e4ccfb6e657f97fd62106173bea9bb47e7e8
Evgeny Semenov <00semen00@gmail.com>
2025-07-31 12:07:50 +0300

    Started problem 3014 from leetcode "Minimum Number of Pushes to Type Word I"
    https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-i/description/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode3014/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode3014;
+
+public class Solution {
+
+    public int minimumPushes(String word) {
+        int result = 0;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode3014/SolutionTest.java
@@ -0,0 +1,30 @@
+package org.just.leetcode.leetcode3014;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String arg1, int expected) {
+        int actual = solution.minimumPushes(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("abcde", 5),
+                Arguments.of("xycdefghij", 12)
+        );
+    }
+}
