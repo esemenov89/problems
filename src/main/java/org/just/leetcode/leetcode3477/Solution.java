37494526d87117d3be7bd2f6ce782913fb10e003
Evgeny Semenov <00semen00@gmail.com>
2025-08-05 15:12:48 +0300

    Solved problem 3477 from leetcode "Fruits Into Baskets II"
    https://leetcode.com/problems/fruits-into-baskets-ii/?envType=daily-question&envId=2025-08-05

--- src/main/java/org/just/leetcode/leetcode3477/Solution.java
+++ src/main/java/org/just/leetcode/leetcode3477/Solution.java
@@ -3,7 +3,18 @@ package org.just.leetcode.leetcode3477;
 public class Solution {
 
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
-        int result = 0;
+        int result;
+        int countPlacedTypes = 0;
+        for (int i = 0; i < fruits.length; i++) {
+            for (int j = 0; j < baskets.length; j++) {
+                if (fruits[i] <= baskets[j]) {
+                    countPlacedTypes++;
+                    baskets[j] = -1;
+                    break;
+                }
+            }
+        }
+        result = fruits.length - countPlacedTypes;
         return result;
     }
 }
--- src/test/java/org/just/leetcode/leetcode3477/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode3477/SolutionTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class SolutionTest {
@@ -24,8 +25,23 @@ public class SolutionTest {
     private static Stream<Arguments> provideArguments() {
         return Stream.of(
                 Arguments.of(new int[]{4, 2, 5}, new int[]{3, 5, 4}, 1),
-                Arguments.of(new int[]{4, 2, 5}, new int[]{3, 5, 4}, 1),
-                Arguments.of(new int[]{4, 2, 5}, new int[]{3, 5, 4}, 1)
+                Arguments.of(new int[]{3, 6, 1}, new int[]{6, 4, 7}, 0),
+                Arguments.of(generateArray(), generateArray(), 0),
+                Arguments.of(generateArray(), generateArray2(), 998)
         );
+    }
+
+    private static int[] generateArray() {
+        int[] array = new int[1000];
+        Arrays.fill(array, 1);
+        return array;
+    }
+
+    private static int[] generateArray2() {
+        int[] array = new int[1000];
+        Arrays.fill(array, 0);
+        array[998] = 1;
+        array[999] = 1;
+        return array;
     }
 }
