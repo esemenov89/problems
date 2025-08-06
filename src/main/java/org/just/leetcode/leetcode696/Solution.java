eb8ab7e3abf8d02423ab3f6dfbcabcac1db1311d
Evgeny Semenov <00semen00@gmail.com>
2025-08-06 15:56:19 +0300

    Solved problem 696 from leetcode "Count Binary Substrings"
    https://leetcode.com/problems/count-binary-substrings/?envType=problem-list-v2&envId=string

--- src/main/java/org/just/leetcode/leetcode696/Solution.java
+++ src/main/java/org/just/leetcode/leetcode696/Solution.java
@@ -1,9 +1,27 @@
 package org.just.leetcode.leetcode696;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
     public int countBinarySubstrings(String s) {
-        int result = 0;
-        return result;
+        int currentIndex = 0;
+        int length = s.length();
+        List<Integer> groups = new ArrayList<>();
+        while (currentIndex < length) {
+            int count = 1;
+            while (currentIndex + 1 < length && s.charAt(currentIndex + 1) == s.charAt(currentIndex)) {
+                currentIndex++;
+                count++;
+            }
+            groups.add(count);
+            currentIndex++;
+        }
+        int totalSubstrings = 0;
+        for (int i = 1; i < groups.size(); i++) {
+            totalSubstrings += Math.min(groups.get(i - 1), groups.get(i));
+        }
+        return totalSubstrings;
     }
 }
--- src/test/java/org/just/leetcode/leetcode696/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode696/SolutionTest.java
@@ -23,9 +23,12 @@ public class SolutionTest {
 
     private static Stream<Arguments> provideArguments() {
         return Stream.of(
+                Arguments.of("1111100000", 5),
                 Arguments.of("00110011", 6),
                 Arguments.of("10101", 4),
-                Arguments.of("10101", 4)
+                Arguments.of("010101", 5),
+                Arguments.of("01".repeat(100000), 200000),
+                Arguments.of("1".repeat(50000) + "0".repeat(50000), 50000)
         );
     }
 }
