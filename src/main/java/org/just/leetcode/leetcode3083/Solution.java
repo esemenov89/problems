3d3e9b9a4b6b04fa2e909182bf2935b0c0d39ee7
Evgeny Semenov <00semen00@gmail.com>
2025-08-07 17:10:03 +0300

    Started problem 2083 from leetcode "Existence of a Substring in a String and Its Reverse"
    https://leetcode.com/problems/existence-of-a-substring-in-a-string-and-its-reverse/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode3083/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode3083;
+
+public class Solution {
+
+    public boolean isSubstringPresent(String s) {
+        boolean result = false;
+        return result;
+    }
+}
