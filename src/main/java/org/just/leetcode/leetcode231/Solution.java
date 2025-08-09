b486e7b4555490af6d90066d4ab912c652c00901
Evgeny Semenov <00semen00@gmail.com>
2025-08-09 08:53:37 +0300

    Solved problem 231 from leetcode "Power of Two"
    https://leetcode.com/problems/power-of-two/description/?envType=daily-question&envId=2025-08-09

--- src/main/java/org/just/leetcode/leetcode231/Solution.java
+++ src/main/java/org/just/leetcode/leetcode231/Solution.java
@@ -3,7 +3,8 @@ package org.just.leetcode.leetcode231;
 public class Solution {
 
     public boolean isPowerOfTwo(int n) {
-        boolean result = false;
-        return n > 0 && (n & (n - 1)) == 0;
+        boolean result;
+        result = n > 0 && (n & (n - 1)) == 0;
+        return result;
     }
 }
--- src/test/java/org/just/leetcode/leetcode231/SolutionTest.java
+++ src/test/java/org/just/leetcode/leetcode231/SolutionTest.java
@@ -25,7 +25,9 @@ public class SolutionTest {
         return Stream.of(
                 Arguments.of(1, true),
                 Arguments.of(16, true),
-                Arguments.of(3, false)
+                Arguments.of(3, false),
+                Arguments.of(5, false),
+                Arguments.of(4, true)
         );
     }
 }
