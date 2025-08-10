af6e3c1e6267f921a293b22426ffb5e7b4589a59
Evgeny Semenov <00semen00@gmail.com>
2025-08-10 06:40:32 +0300

    Started problem 869 from leetcode "Reordered Power of 2"
    https://leetcode.com/problems/reordered-power-of-2/?envType=daily-question&envId=2025-08-10

---
+++ src/main/java/org/just/leetcode/leetcode869/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode869;
+
+public class Solution {
+
+    public boolean reorderedPowerOf2(int n) {
+        boolean result = false;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode869/SolutionTest.java
@@ -0,0 +1,33 @@
+package org.just.leetcode.leetcode869;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(int arg1, boolean expected) {
+        boolean actual = solution.reorderedPowerOf2(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(1, true),
+                Arguments.of(16, true),
+                Arguments.of(3, false),
+                Arguments.of(5, false),
+                Arguments.of(4, true)
+        );
+    }
+}
