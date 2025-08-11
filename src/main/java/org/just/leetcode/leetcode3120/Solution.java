80977ad698a1ee2a905f8e895d619f0e2d838596
Evgeny Semenov <00semen00@gmail.com>
2025-08-11 13:51:59 +0300

    Started problem 3120 from leetcode "Count the Number of Special Characters I"
    https://leetcode.com/problems/count-the-number-of-special-characters-i/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode3120/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode3120;
+
+public class Solution {
+
+    public int numberOfSpecialChars(String word) {
+        int result = 0;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode3120/SolutionTest.java
@@ -0,0 +1,33 @@
+package org.just.leetcode.leetcode3120;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String arg1, int expected) {
+        int actual = solution.numberOfSpecialChars(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("aaAbcBC", 3),
+                Arguments.of("abc", 0),
+                Arguments.of("abBCab", 1),
+                Arguments.of("aaAbcBC", 3),
+                Arguments.of("aaAbcBC", 3)
+        );
+    }
+}
