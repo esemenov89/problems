108e98238dc581de2ede5a3c5c3ebf4a3ae01219
Evgeny Semenov <00semen00@gmail.com>
2025-08-12 14:48:13 +0300

    Started problem 383 from leetcode "Ransom Note"
    https://leetcode.com/problems/ransom-note/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode383/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode383;
+
+public class Solution {
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+        boolean result = false;
+        return result;
+    }
+}
