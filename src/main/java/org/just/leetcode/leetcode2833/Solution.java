ad7e016924f5fe970af272f4748e7bac69cef4cf
Evgeny Semenov <00semen00@gmail.com>
2025-08-19 17:10:47 +0300

    Started problem 2833 from leetcode "Furthest Point From Origin"
    https://leetcode.com/problems/furthest-point-from-origin/description/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode2833/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode2833;
+
+public class Solution {
+
+    public int furthestDistanceFromOrigin(String moves) {
+        int result = 0;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode2833/SolutionTest.java
@@ -0,0 +1,31 @@
+package org.just.leetcode.leetcode2833;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String arg1, int expected) {
+        int actual = solution.furthestDistanceFromOrigin(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("L_RL__R", 3),
+                Arguments.of("_R__LL_", 5),
+                Arguments.of("_______", 7)
+        );
+    }
+}
