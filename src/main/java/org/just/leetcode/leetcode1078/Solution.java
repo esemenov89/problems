f9f98982b36ea32ce2ec04ca5c6c288e5c14decc
Evgeny Semenov <00semen00@gmail.com>
2025-09-01 14:00:02 +0300

    Started problem 1078 from leetcode "Occurrences After Bigram"
    https://leetcode.com/problems/occurrences-after-bigram/description/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode1078/Solution.java
@@ -0,0 +1,12 @@
+package org.just.leetcode.leetcode1078;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public String[] findOcurrences(String text, String first, String second) {
+        String[] result = new String[2];
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode1078/SolutionTest.java
@@ -0,0 +1,32 @@
+package org.just.leetcode.leetcode1078;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String arg1, String arg2, String arg3, String[] expected) {
+        String[] actual = solution.findOcurrences(arg1, arg2, arg3);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("alice is a good girl she is a good student", "a", "good", new String[]{"girl", "student"}),
+                Arguments.of("we will we will rock you", "we", "will", new String[]{"we", "rock"}),
+                Arguments.of("alice is a good girl she is a good student", "a", "good", new String[]{"girl", "student"}),
+                Arguments.of("alice is a good girl she is a good student", "a", "good", new String[]{"girl", "student"})
+        );
+    }
+}
