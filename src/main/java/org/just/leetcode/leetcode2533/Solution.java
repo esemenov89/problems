a5503534321f788166c897f185650373c18c2948
Evgeny Semenov <00semen00@gmail.com>
2025-09-02 16:56:52 +0300

    Solved problem 2533 from leetcode "Separate the Digits in an Array"
    https://leetcode.com/problems/separate-the-digits-in-an-array/?envType=problem-list-v2&envId=array

--- src/main/java/org/just/leetcode/leetcode2533/Solution.java
+++ src/main/java/org/just/leetcode/leetcode2533/Solution.java
@@ -1,9 +1,27 @@
 package org.just.leetcode.leetcode2533;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
     public int[] separateDigits(int[] nums) {
-        int[] result = new int[nums.length];
+        int[] result;
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            if (num < 10) {
+                list.add(num);
+            } else {
+                String s = String.valueOf(num);
+                for (int j = 0; j < s.length(); j++) {
+                    list.add(Integer.parseInt(s.substring(j, j + 1)));
+                }
+            }
+        }
+        result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
         return result;
     }
 }
