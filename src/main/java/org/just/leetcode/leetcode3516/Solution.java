ff7741eb2db8b4087839690a0b318c7a3c93bb13
Evgeny Semenov <00semen00@gmail.com>
2025-09-04 14:48:36 +0300

    Started problem 3516 from leetcode "Find Closest Person"
    https://leetcode.com/problems/find-closest-person/?envType=daily-question&envId=2025-09-04

---
+++ src/main/java/org/just/leetcode/leetcode3516/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode3516;
+
+public class Solution {
+
+    public int findClosest(int x, int y, int z) {
+        int result = 0;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode3516/SolutionTest.java
@@ -0,0 +1,31 @@
+package org.just.leetcode.leetcode3516;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(int arg1, int arg2, int arg3, int expected) {
+        int actual = solution.findClosest(arg1, arg2, arg3);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(2, 7, 4, 1),
+                Arguments.of(2, 7, 4, 1),
+                Arguments.of(2, 7, 4, 1)
+        );
+    }
+}
