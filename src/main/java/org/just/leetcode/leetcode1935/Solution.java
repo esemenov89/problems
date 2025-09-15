8e2336e64b51a1935bdb15079c2870330e358360
Evgeny Semenov <00semen00@gmail.com>
2025-09-15 13:57:34 +0300

    Solved problem 1935 from leetcode "Maximum Number of Words You Can Type"
    https://leetcode.com/problems/maximum-number-of-words-you-can-type/?envType=daily-question&envId=2025-09-15

--- src/main/java/org/just/leetcode/leetcode1935/Solution.java
+++ src/main/java/org/just/leetcode/leetcode1935/Solution.java
@@ -4,6 +4,21 @@ public class Solution {
 
     public int canBeTypedWords(String text, String brokenLetters) {
         int result = 0;
+        char[] chars = brokenLetters.toCharArray();
+        String[] words = text.split(" ");
+        boolean contains;
+        for (String word : words) {
+            contains = false;
+            for (char aChar : chars) {
+                if (word.contains(String.valueOf(aChar))) {
+                    contains = true;
+                    break;
+                }
+            }
+            if (!contains) {
+                result++;
+            }
+        }
         return result;
     }
 }
