4d689916451d72f77f8464acbc799bf5db57d6b8
Evgeny Semenov <00semen00@gmail.com>
2025-09-17 09:15:44 +0300

    Solved problem 1763 from leetcode "Longest Nice Substring"
    https://leetcode.com/problems/longest-nice-substring/?envType=problem-list-v2&envId=string

--- src/main/java/org/just/leetcode/leetcode1763/Solution.java
+++ src/main/java/org/just/leetcode/leetcode1763/Solution.java
@@ -1,9 +1,33 @@
 package org.just.leetcode.leetcode1763;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
 
     public String longestNiceSubstring(String s) {
-        String result = "";
-        return result;
+        int stringLength = s.length();
+        int startIndex = -1;
+        int maxLength = 0;
+
+        for (int i = 0; i < stringLength; ++i) {
+            Set<Character> charSet = new HashSet<>();
+            for (int j = i; j < stringLength; ++j) {
+                charSet.add(s.charAt(j));
+                boolean isNice = true;
+                for (char ch : charSet) {
+                    char counterpart = (char) (ch ^ 32);
+                    if (!charSet.contains(ch) || !charSet.contains(counterpart)) {
+                        isNice = false;
+                        break;
+                    }
+                }
+                if (isNice && maxLength < j - i + 1) {
+                    maxLength = j - i + 1;
+                    startIndex = i;
+                }
+            }
+        }
+        return startIndex == -1 ? "" : s.substring(startIndex, startIndex + maxLength);
     }
 }
