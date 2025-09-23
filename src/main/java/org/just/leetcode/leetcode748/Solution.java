cb0571db4d119d72ad4f6160520a6f9f89647913
Evgeny Semenov <00semen00@gmail.com>
2025-09-23 12:02:37 +0300

    Started problem 748 from leetcode "Shortest Completing Word"
    https://leetcode.com/problems/shortest-completing-word/description/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode748/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode748;
+
+public class Solution {
+
+    public String shortestCompletingWord(String licensePlate, String[] words) {
+        String result = "";
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode748/SolutionTest.java
@@ -0,0 +1,31 @@
+package org.just.leetcode.leetcode748;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String arg1, String[] arg2, String expected) {
+        String actual = solution.shortestCompletingWord(arg1, arg2);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("1s3 PSt", new String[] {"step","steps","stripe","stepple"}, "steps"),
+                Arguments.of("1s3 456", new String[] {"looks","pest","stew","show"}, "pest"),
+                Arguments.of("1s3 PSt", new String[] {"step","steps","stripe","stepple"}, "steps")
+        );
+    }
+}
