bc074e93ce7450bc537359c5f7376dcf663974ac
Evgeny Semenov <00semen00@gmail.com>
2025-09-24 13:23:12 +0300

    Started problem 1869 from leetcode "Longer Contiguous Segments of Ones than Zeros"
    https://leetcode.com/problems/longer-contiguous-segments-of-ones-than-zeros/?envType=problem-list-v2&envId=string

---
+++ src/main/java/org/just/leetcode/leetcode1869/Solution.java
@@ -0,0 +1,9 @@
+package org.just.leetcode.leetcode1869;
+
+public class Solution {
+
+    public boolean checkZeroOnes(String s) {
+        boolean result = false;
+        return result;
+    }
+}
---
+++ src/test/java/org/just/leetcode/leetcode1869/SolutionTest.java
@@ -0,0 +1,32 @@
+package org.just.leetcode.leetcode1869;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testSolution(String arg1, boolean expected) {
+        boolean actual = solution.checkZeroOnes(arg1);
+
+        assertThat(actual, is(expected));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("1101", true),
+                Arguments.of("111000", false),
+                Arguments.of("110100010", false),
+                Arguments.of("1101", true)
+        );
+    }
+}
